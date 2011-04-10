7fc47085264ff183a7a68198fc603c314d0be0b3
Michael Mathews <micmath@gmail.com>
2011-04-10 09:54:07 +0200

    Added addition tests for the @default tag.

--- test/cases/defaulttag.js
+++ test/cases/defaulttag.js
@@ -14,6 +14,21 @@ var response = 'ok';
 var rcode = 200;
 
 /**
+    @default
+ */
+var rvalid = true;
+
+/**
+    @default
+ */
+var rerrored = false;
+
+/**
     @default the parent window
  */
 var win = getParentWindow();
+
+/**
+    @default
+ */
+var header = getHeaders(request);
