ce3671cb436d0618e3c1ad45d0bdb68ca26844b1
Michael Mathews <micmath@gmail.com>
2011-08-29 20:52:40 +0100

    Change to CSS on default template to hide the variation name, when displaying multiple symbols with the same name. Added test case file with example of more complex variations usage.

---
+++ test/cases/variations2.js
@@ -0,0 +1,36 @@
+/**
+* Describe the namespace.
+* @namespace dollar(1)
+*/
+
+/**
+* Describe the constructor.
+* @constructor dollar(2)
+* @param {object} [options]
+*/
+
+/**
+* Describe the global function.
+* @function dollar(3)
+* @param {string} selector - The selector string.
+*/
+dollar = function(a, b) {
+};
+
+/**
+* Describe the instance method.
+* @instance
+* @function empty
+* @memberof dollar(2)
+*/
+dollar.prototype.empty = function() {
+}
+
+/**
+* Describe the static method.
+* @function klass
+* @memberof dollar(1)
+* @param {string} name
+*/
+dollar.klass = function(name) {
+};
