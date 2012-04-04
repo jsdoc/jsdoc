130bef796a5a28ef05a0cf580eb0e822be25c266
kristiancalhoun <kristian.calhoun@gmail.com>
2012-04-04 10:06:19 -0400

    Added custom tags plugin.

---
+++ plugins/customTags.js
@@ -0,0 +1,29 @@
+/**
+    @overview Define custom tags for Geoscope
+    @module plugins/customTags
+    @author Kristian Calhoun <kristian.calhoun@gmail.com>
+ */
+
+exports.defineTags = function(dictionary) {
+
+	dictionary.defineTag('enumeration', {
+        onTagged: function(doclet, tag) {
+            doclet.addTag('kind', 'class');
+        }
+    });
+	
+	dictionary.defineTag('performance', {
+		mustHaveValue: true,
+        onTagged: function(doclet, tag) {
+            if (!doclet.performance) { doclet.performance = []; }
+            doclet.performance.push(tag.value);
+        }
+    });
+	
+	dictionary.defineTag('glsl', {
+		onTagged: function(doclet, tag) {
+			doclet.addTag('kind', 'glsl');
+			doclet.filename = doclet.name;
+		}
+	}).synonym('glslStruct').synonym('glslUniform').synonym('glslConstant').synonym('glslFunction');
+};
