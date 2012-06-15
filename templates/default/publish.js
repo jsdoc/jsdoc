eb0186b96aeedffdc00625b32d62674acee00a8b
Michael Mathews <micmath@gmail.com>
2012-06-15 07:57:19 +0100

    Do not show a header for Classes if the documentation contains no classes.

--- templates/default/publish.js
+++ templates/default/publish.js
@@ -272,14 +272,18 @@
             return a.name > b.name;
         });
         if (classNames.length) {
-            nav += '<h3>Classes</h3><ul>';
+            var moduleClasses = 0;
             classNames.forEach(function(c) {
                 var moduleSameName = find({kind: 'module', longname: c.longname});
                 if (moduleSameName.length) {
-                    c.name = c.name.replace('module:', 'require(')+')';
+                    c.name = c.name.replace('module:', 'require("')+'")';
+                    moduleClasses++;
                     moduleSameName[0].module = c;
                 }
-                
+                if (moduleClasses !== -1 && moduleClasses < classNames.length) {
+                    nav += '<h3>Classes</h3><ul>';
+                    moduleClasses = -1;
+                }
                 if ( !hasOwnProperty.call(seen, c.longname) ) nav += '<li>'+linkto(c.longname, c.name)+'</li>';
                 seen[c.longname] = true;
             });
