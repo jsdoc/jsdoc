378572ae3243cf2a3bbd7604f57ba66a337dee2d
Ben Blank <ben.blank@gmail.com>
2012-06-16 14:13:24 -0700

    `SHIFT` doesn't seeem to affect `%*`

--- jsdoc.cmd
+++ jsdoc.cmd
@@ -21,17 +21,25 @@ IF NOT "%_URLPATH%"=="%_URLPATH: =%" GOTO ESCAPE_SPACE
 IF [%1]==[--debug] (
     ECHO Running Debug
     SET CMD=org.mozilla.javascript.tools.debugger.Main -debug
+
+    REM `SHIFT` doesn't affect %*
+    :COLLECT_ARGS
+    IF [%2]==[] GOTO LAST_ARG
+    SET ARGS=%ARGS% %2
     SHIFT
+    GOTO COLLECT_ARGS
 ) ELSE (
     SET CMD=org.mozilla.javascript.tools.shell.Main
+    SET ARGS=%*
 )
+:LAST_ARG
 
 IF [%1]==[-T] (
     ECHO Running Tests
-    java -classpath "%_BASEPATH%/lib/js.jar" %CMD% -opt -1 -modules "%_URLPATH%/node_modules" -modules "%_URLPATH%/rhino_modules" -modules "%_URLPATH%" "%_BASEPATH%/jsdoc.js" %* --dirname="%_BASEPATH%/
+    java -classpath "%_BASEPATH%/lib/js.jar" %CMD% -opt -1 -modules "%_URLPATH%/node_modules" -modules "%_URLPATH%/rhino_modules" -modules "%_URLPATH%" "%_BASEPATH%/jsdoc.js" %ARGS% --dirname="%_BASEPATH%/
 ) ELSE (
     REM normal mode should be quiet
-    java -classpath "%_BASEPATH%/lib/js.jar" %CMD% -modules "%_URLPATH%/node_modules" -modules "%_URLPATH%/rhino_modules" -modules "%_URLPATH%" "%_BASEPATH%/jsdoc.js" %* --dirname="%_BASEPATH%/
+    java -classpath "%_BASEPATH%/lib/js.jar" %CMD% -modules "%_URLPATH%/node_modules" -modules "%_URLPATH%/rhino_modules" -modules "%_URLPATH%" "%_BASEPATH%/jsdoc.js" %ARGS% --dirname="%_BASEPATH%/
 )
 
 ENDLOCAL
