e7dd88da46eff7d085b1a0063a0355d31adc3dbd
Ben Blank <ben.blank@gmail.com>
2012-06-16 11:06:19 -0700

    restore parity between `jsdoc` and `jsdoc.cmd`

--- jsdoc.cmd
+++ jsdoc.cmd
@@ -18,8 +18,20 @@ SET _URLPATH=%_URLPATH%%%20%_TRAILING%
 IF NOT "%_URLPATH%"=="%_URLPATH: =%" GOTO ESCAPE_SPACE
 :NO_SPACES
 
-java -classpath "%_BASEPATH%/lib/js.jar" org.mozilla.javascript.tools.shell.Main -modules "%_URLPATH%/node_modules" -modules "%_URLPATH%/rhino_modules" -modules "%_URLPATH%" "%_BASEPATH%/jsdoc.js" %* --dirname="%_BASEPATH%/
+IF [%1]==[--debug] (
+    ECHO Running Debug
+    SET CMD=org.mozilla.javascript.tools.debugger.Main -debug
+    SHIFT
+) ELSE (
+    SET CMD=org.mozilla.javascript.tools.shell.Main
+)
 
-REM java -classpath "%_BASEPATH%/lib/js.jar" org.mozilla.javascript.tools.debugger.Main -debug -modules "%_URLPATH%/node_modules/" -modules "%_URLPATH%/rhino_modules/" -modules "%_URLPATH%/" "%_BASEPATH%/jsdoc.js" %* --dirname="%_BASEPATH%/
+IF [%1]==[-T] (
+    ECHO Running Tests
+    java -classpath "%_BASEPATH%/lib/js.jar" %CMD% -opt -1 -modules "%_URLPATH%/node_modules" -modules "%_URLPATH%/rhino_modules" -modules "%_URLPATH%" "%_BASEPATH%/jsdoc.js" %* --dirname="%_BASEPATH%/
+) ELSE (
+    REM normal mode should be quiet
+    java -classpath "%_BASEPATH%/lib/js.jar" %CMD% -modules "%_URLPATH%/node_modules" -modules "%_URLPATH%/rhino_modules" -modules "%_URLPATH%" "%_BASEPATH%/jsdoc.js" %* --dirname="%_BASEPATH%/
+)
 
 ENDLOCAL
