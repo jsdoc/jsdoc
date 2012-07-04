ee858cc89475e3f67461bf85a04381a560fff972
Jeff Williams <jeffrey.l.williams@gmail.com>
2012-07-03 18:31:03 -0700

    replace "unsafe" character with HTML entity
    this fixes a JSHint error that cannot be suppressed.

--- rhino_modules/jsdoc/tutorial/resolver.js
+++ rhino_modules/jsdoc/tutorial/resolver.js
@@ -1,6 +1,6 @@
 /**
     @overview
-    @author Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
+    @author Rafa&#322; Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
     @license Apache License 2.0 - See file 'LICENSE.md' in this project.
  */
 
