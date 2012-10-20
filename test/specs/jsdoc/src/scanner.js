371506038f24e30c144a42e729b9483bb98f1fbc
Jeff Williams <jeffrey.l.williams@gmail.com>
2012-10-19 21:51:39 -0700

    fix path issues that caused the test to fail on Windows (#168)

--- test/specs/jsdoc/src/scanner.js
+++ test/specs/jsdoc/src/scanner.js
@@ -1,19 +1,21 @@
+/*global describe: true, env: true, expect: true, it: true */
 describe("jsdoc/src/scanner", function() {
     var scanner = new (require('jsdoc/src/scanner').Scanner)(),
         filter = new (require('jsdoc/src/filter').Filter)({
             includePattern: new RegExp(".+\\.js(doc)?$"),
             excludePattern: new RegExp("(^|\\/)_")
         }),
-        sourceFiles = scanner.scan([env.dirname+'/test/fixtures/src/'], 3, filter);
+        path = require('path'),
+        sourceFiles = scanner.scan([path.join(env.dirname, 'test', 'fixtures', 'src')], 3, filter);
 
     sourceFiles = sourceFiles.map(function($) {
-        return $.replace(env.dirname, '');
+        return path.relative(env.dirname, $);
     });
 
     it("should return the correct source files", function() {
         expect(sourceFiles.length).toEqual(3);
-        expect(sourceFiles.indexOf("/test/fixtures/src/one.js")).toBeGreaterThan(-1);
-        expect(sourceFiles.indexOf("/test/fixtures/src/two.js")).toBeGreaterThan(-1);
-        expect(sourceFiles.indexOf("/test/fixtures/src/dir1/three.js")).toBeGreaterThan(-1);
+        expect(sourceFiles.indexOf(path.join('test', 'fixtures', 'src', 'one.js'))).toBeGreaterThan(-1);
+        expect(sourceFiles.indexOf(path.join('test', 'fixtures', 'src', 'two.js'))).toBeGreaterThan(-1);
+        expect(sourceFiles.indexOf(path.join('test', 'fixtures', 'src', 'dir1', 'three.js'))).toBeGreaterThan(-1);
     });
 });
