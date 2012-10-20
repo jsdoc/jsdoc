7ebf9c59764243937621656cdb71c48362603856
Matthew Kastor <matthewkastor@gmail.com>
2012-10-20 18:20:33 -0400

    prettyPrintSource now uses it's own template
    now that the templates.js file is fixed, plugins can use templates
    without trashing everything.

--- plugins/prettyPrintSource.js
+++ plugins/prettyPrintSource.js
@@ -1,6 +1,5 @@
-/*global env: true */
 /**
- * @file Adds pretty printed source code to
+ * @file Adds pretty printed source code to 
  * the output files.
  * @author <a href="mailto:matthewkastor@google.com">Matthew Christopher Kastor-Inare III</a>
  * Hial Atropa!!
@@ -10,11 +9,12 @@
 exports.handlers = {
     beforeParse: function(e) {
         
-        function bs2fs(text) {
+        function backslashToForwardslash(text) {
             return text.replace(/\\/g, '/');
         }
         
         function toRelativePath(text) {
+            text  = backslashToForwardslash(text);
             return text.replace(/^.*:/, '');
         }
         
@@ -27,20 +27,29 @@ exports.handlers = {
         }
         
         function noDotPrefix(text) {
-            return text.replace(/^\./, '');
+            return text.replace(/^\.+/, '');
+        }
+        
+        function noDotSuffix(text) {
+            return text.replace(/\.+$/, '');
+        }
+        
+        function trimDots(text) {
+            text  = noDotPrefix(text);
+            return noDotSuffix(text);
+        }
+        
+        function relativePathToNamespace (text) {
+            text  = slashesCollapseToDots(text);
+            return trimDots(text);
         }
         
         function makeOutputFileName(sourceFileName, extension) {
             extension = extension || '';
             
-            var out;
-            
-            out  = toRelativePath(sourceFileName);
-            out  = slashesCollapseToDots(out);
-            out  = noDotPrefix(out);
-            out += extension;
-            
-            return out;
+            sourceFileName  = toRelativePath(sourceFileName);
+            sourceFileName  = relativePathToNamespace(sourceFileName);
+            return sourceFileName += extension;
         }
         
         function getOutputDirectory() {
@@ -48,65 +57,29 @@ exports.handlers = {
             var out;
             
             out  = path.resolve(env.opts.destination);
-            out  = bs2fs(out);
-            out  = suffixFsCollapse(out + '/');
-            
-            return out;
+            out  = backslashToForwardslash(out);
+            return suffixFsCollapse(out + '/');
         }
         
-        function generateHighlightedSourceFile(outDir, outfile, sourceContent) {
-            var fs = require('fs');
-            var outsource;
-            
-            outsource  = '<!DOCTYPE html>\n';
-            outsource += '  <head>\n';
-            outsource += '      <meta charset="utf-8">\n';
-            outsource += '      <title>Source of : ' + outfile + '</title>\n';
-            outsource += '      <style type="text/css">\n';
-            outsource += '          pre, code\n';
-            outsource += '          {\n';
-            outsource += '              white-space: pre-wrap;\n';
-            outsource += '          }\n';
-            outsource += '      </style>\n';
-            outsource += '      <script src="scripts/prettify/prettify.js"> </script>\n';
-            outsource += '      <!--[if lt IE 9]>\n';
-            outsource += '          <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>\n';
-            outsource += '      <![endif]-->\n';
-            outsource += '      <link type="text/css" title="desert" rel="stylesheet" href="styles/desert.css">\n';
-            outsource += '      <link type="text/css" title="sunburst" rel="alternate stylesheet" href="styles/sunburst.css">\n';
-            outsource += '      <link type="text/css" title="prettify" rel="alternate stylesheet" href="styles/prettify.css">\n';
-            outsource += '      <link type="text/css" title="prettify-jsdoc" rel="alternate stylesheet" href="styles/prettify-jsdoc.css">\n';
-            outsource += '  </head>\n';
-            outsource += '  <body>\n';
-            outsource += '      <pre class="prettyprint lang-js linenums">\n';
-            outsource += sourceContent;
-            outsource += '      </pre>\n';
-            outsource += '      <script>prettyPrint();</script>\n';
-            outsource += '  </body>\n';
-            outsource += '</html>\n';
-            
-            fs.mkPath(outDir);
-            fs.writeFileSync(outfile, outsource);
+        function generateHighlightedSourceFile(outputDirectory, outputFileName, sourceCode) {
+            var prettyPrinter = require('templates/prettyPrintSource/publish');
+            prettyPrinter.publish(outputDirectory, outputFileName, sourceCode);
         }
         
         function main() {
+            var outputDirectory = getOutputDirectory();
+            var outputFileName  = makeOutputFileName(e.filename);
+            var sourceCode      = e.source;
             
-            var sourceFileName= bs2fs(e.filename);
-            var sourceContent = e.source;
-            var outFileName   = makeOutputFileName(sourceFileName, '.html');
-            var outDir        = getOutputDirectory();
-            var outfile       = outDir + outFileName;
-            
-            /*// Debug
-            print('source file name = ' + sourceFileName);
-            print('output file name = ' + outFileName);
-            print('output directory = ' + outDir);
-            print('output file      = ' + outfile);
-            print('sourceContent    = ' + sourceContent);
+            /*// Debug 
+            print('source file name = ' + e.filename);
+            print('output file name = ' + outputFileName);
+            print('output directory = ' + outputDirectory);
+            print('sourceCode       = ' + sourceCode);
             //*/
             
             // write source to file with a unique name
-            generateHighlightedSourceFile(outDir, outfile, sourceContent);
+            generateHighlightedSourceFile(outputDirectory, outputFileName, sourceCode);
             // link to source in documentation somehow
         }
         
---
+++ templates/prettyPrintSource/publish.js
@@ -0,0 +1,41 @@
+/**
+ * Turn the data about your docs into file output.
+ * @param {TAFFY} data A TaffyDB collection representing
+ *                       all the symbols documented in your code.
+ * @param {object} opts An object with options information.
+ * @param {Tutorial} tutorialResolverRoot An object with information about included tutorials
+ * @see http://www.taffydb.com/
+ * @see http://www.taffydb.com/workingwithdata
+ * @see http://underscorejs.org/#template
+ * @see http://nodejs.org/api/fs.html
+ * @see http://nodejs.org/api/path.html
+ */
+exports.publish = function(outputDirectory, outputFileName, sourceCode) {
+    
+    var fs                 = require('fs');
+    var path               = require('path');
+    var helper             = require('jsdoc/util/templateHelper');
+    var template           = require('jsdoc/template');
+    
+    outputDirectory        = path.resolve(outputDirectory);
+    var outputFile         = path.join(outputDirectory, outputFileName + '.html');
+    var templateName       = 'layout.tmpl';
+    var templatePath       = path.resolve(env.dirname + '/templates/prettyPrintSource/tmpl');
+    var templateData       = {};
+    templateData.title     = 'Source of : ' + outputFileName;
+    templateData.sourceCode= helper.htmlsafe(sourceCode);
+    
+    
+    
+    var view               = new template.Template(templatePath);
+    var outputContent      = view.render(templateName, templateData);
+    
+    /*// Debug
+    console.log('templateData    : ' + templateData);
+    console.log('outputDirectory : ' + outputDirectory);
+    console.log('outputFile      : ' + outputFile);
+    console.log('outputContent   : ' + outputContent);
+    //*/
+    fs.mkPath(outputDirectory);
+    fs.writeFileSync(outputFile, outputContent);
+};
