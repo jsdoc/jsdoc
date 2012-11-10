f205e2bde105c3b7a5244ec0ac1733cee111dbca
Jeff Williams <jeffrey.l.williams@gmail.com>
2012-11-10 11:19:30 -0800

    don't swallow uncaught exceptions (#257)

--- jsdoc.js
+++ jsdoc.js
@@ -426,12 +426,12 @@ function main() {
 try { main(); }
 catch(e) {
      if (e.rhinoException != null) {
-         e.rhinoException.printStackTrace();
+        // TODO: print to stderr
+        e.rhinoException.printStackTrace();
      } else {
         throw e;
     }
 }
 finally {
     env.run.finish = new Date();
-    process.exit(0);
 }
