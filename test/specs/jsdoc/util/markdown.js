24a3c6b448332986ef45c3126ef1f9ef2d2b1e72
mathematicalcoffee <mathematical.coffee@gmail.com>
2012-12-28 23:53:07 +1000

    filled out markdown tests for getParser to test asking for specific parsers

--- test/specs/jsdoc/util/markdown.js
+++ test/specs/jsdoc/util/markdown.js
@@ -38,20 +38,74 @@ describe('jsdoc/util/markdown', function() {
                 delete env.conf.markdown;
             }
         }
-		xit('should retrieve a function when called with default settings', function() {
-			// TODO
+
+		it('should retrieve a function when called with default settings', function() {
+            var storage = setMarkdownConf({parser: 'evilstreak'});
+
+            var parser = markdown.getParser();
+            expect(typeof parser).toEqual('function');
+
+            setMarkdownConf({parser: 'gfm'});
+            parser = markdown.getParser();
+            expect(typeof parser).toEqual('function');
+
+            restoreMarkdownConf(storage);
 		});
 
-		xit('should use the evilstreak parser when requested', function() {
-			// TODO
+		it('should use the evilstreak parser when requested', function() {
+            var storage = setMarkdownConf({parser: 'evilstreak'});
+            var parser = markdown.getParser();
+            expect(parser._parser).toEqual('markdown');
+            restoreMarkdownConf(storage);
 		});
 
-		xit('should use the GFM parser when requested', function() {
-			// TODO
+		it('should use the GFM parser when requested', function() {
+            var storage = setMarkdownConf({parser: 'gfm'});
+            var parser = markdown.getParser();
+            expect(parser._parser).toEqual('github-flavored-markdown');
+            restoreMarkdownConf(storage);
 		});
 
-		xit('should convert GitHub repo references to links when the correct options are defined', function() {
-			// TODO
+        it('should use GFM parser when parser is not specified but github owner and repo are', function() {
+            var storage = setMarkdownConf({githubRepoOwner: 'jsdoc', githubRepoName: 'jsdoc3'});
+            var parser = markdown.getParser();
+            expect(parser._parser).toEqual('github-flavored-markdown');
+            restoreMarkdownConf(storage);
+        });
+
+		it('should convert GitHub repo references to links when the correct options are defined', function() {
+            var storage = setMarkdownConf({parser: 'gfm', githubRepoOwner: 'jsdoc', githubRepoName: 'jsdoc3'});
+            var parser = markdown.getParser();
+            expect(parser._parser).toEqual('github-flavored-markdown');
+
+            var sha = '228c940816b5f799a12f83f071a1c67cbb478f39';
+            var sha7 = sha.substr(0, 7);
+
+            // Auto-link sha1 if GitHub.nameWithOwner is defined
+            expect(parser(sha)).toEqual(
+                "<p><a href='http://github.com/jsdoc/jsdoc3/commit/" + sha + "'>" + sha7 + "</a></p>");
+
+            // ** GFM ** Auto-link user@sha1 if GitHub.nameWithOwner is defined
+            expect(parser('mathematicalcoffee@' + sha)).toEqual(
+                "<p><a href='http://github.com/mathematicalcoffee/jsdoc3/commit/" + sha + "'>mathematicalcoffee@" + sha7 + "</a></p>");
+
+            // ** GFM ** Auto-link user/repo@sha1
+            expect(parser('jsdoc/jsdoc3@' + sha)).toEqual(
+                "<p><a href='http://github.com/jsdoc/jsdoc3/commit/" + sha + "'>jsdoc/jsdoc3@" + sha7 + "</a></p>");
+
+            // ** GFM ** Auto-link #issue if GitHub.nameWithOwner is defined
+            expect(parser('Fixes #1')).toEqual(
+                "<p>Fixes <a href='http://github.com/jsdoc/jsdoc3/issues/#issue/1'>#1</a></p>");
+
+            // ** GFM ** Auto-link user#issue if GitHub.nameWithOwner is defined
+            expect(parser('mathematicalcoffee#12')).toEqual(
+                "<p><a href='http://github.com/mathematicalcoffee/jsdoc3/issues/#issue/12'>mathematicalcoffee#12</a></p>");
+
+            // ** GFM ** Auto-link user/repo#issue
+            expect(parser('jsdoc/jsdoc3#1')).toEqual(
+                "<p><a href='http://github.com/jsdoc/jsdoc3/issues/#issue/1'>jsdoc/jsdoc3#1</a></p>");
+            
+            restoreMarkdownConf(storage);
 		});
 
 		it('should not apply formatting to inline tags when the evilstreak parser is enabled', function() {
