4c1611f86f05ac1e69ad40b1bbf7f6ab6764ca81
Jeff Williams <jeffrey.l.williams@gmail.com>
2013-01-23 23:29:52 -0800

    fix class signatures

--- templates/default/publish.js
+++ templates/default/publish.js
@@ -39,7 +39,7 @@ function needsSignature(doclet) {
     var needsSig = false;
 
     // function and class definitions always get a signature
-    if ( doclet.kind === ('function' || 'class') ) {
+    if (doclet.kind === 'function' || doclet.kind === 'class') {
         needsSig = true;
     }
     // typedefs that contain functions get a signature, too
