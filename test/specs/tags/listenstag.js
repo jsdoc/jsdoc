f8d1e877c0826931b910b3bd58235172f2d7bfe9
mathematicalcoffee <mathematical.coffee@gmail.com>
2013-02-15 16:28:50 +1000

    Apply event: namespace to events in @listens

--- test/specs/tags/listenstag.js
+++ test/specs/tags/listenstag.js
@@ -5,7 +5,7 @@ describe("@listens tag", function() {
     it("should create a 'listens' property on the doclet, an array, with the events that are listened to", function() {
         expect(Array.isArray(doc.listens)).toBeTruthy();
         expect(doc.listens.length).toBe(2);
-        expect(doc.listens).toContain('Foo');
-        expect(doc.listens).toContain('Bar');
+        expect(doc.listens).toContain('event:Foo');
+        expect(doc.listens).toContain('event:Bar');
     });
 });
