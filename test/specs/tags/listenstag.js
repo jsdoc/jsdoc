5ffaaff61982f177f532cf7af5b803852db2e14f
mathematicalcoffee <mathematical.coffee@gmail.com>
2013-02-15 16:27:02 +1000

    added @listens tag (need to add to template too)

---
+++ test/specs/tags/listenstag.js
@@ -0,0 +1,11 @@
+describe("@listens tag", function() {
+    var doclet = require('jsdoc/doclet'),
+        doc = new doclet.Doclet('/** An event handler\n@function handler\n@listens Foo\n@listens Bar */', {});
+
+    it("should create a 'listens' property on the doclet, an array, with the events that are listened to", function() {
+        expect(Array.isArray(doc.listens)).toBeTruthy();
+        expect(doc.listens.length).toBe(2);
+        expect(doc.listens).toContain('Foo');
+        expect(doc.listens).toContain('Bar');
+    });
+});
