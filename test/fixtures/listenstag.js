66d4b0623a5e49fccdfae25e1423a6df51be78cf
mathematicalcoffee <mathematical.coffee@gmail.com>
2013-02-26 14:59:52 +1000

    updated @listens test to use a fixture

---
+++ test/fixtures/listenstag.js
@@ -0,0 +1,7 @@
+/** An event handler.
+ * @function
+ * @listens Foo
+ * @listens Bar
+ */
+function handler () {
+}
--- test/specs/tags/listenstag.js
+++ test/specs/tags/listenstag.js
@@ -1,6 +1,6 @@
 describe("@listens tag", function() {
-    var doclet = require('jsdoc/doclet'),
-        doc = new doclet.Doclet('/** An event handler\n@function handler\n@listens Foo\n@listens Bar */', {});
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/listenstag.js'),
+        doc = docSet.getByLongname('handler')[0];
 
     it("should create a 'listens' property on the doclet, an array, with the events that are listened to", function() {
         expect(Array.isArray(doc.listens)).toBeTruthy();
