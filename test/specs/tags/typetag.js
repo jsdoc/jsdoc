482c5aee83dc9336f5e58013860abaa60df4f732
Jeff Williams <jeffrey.l.williams@gmail.com>
2013-03-15 08:51:59 -0700

    partial support for Closure Compiler types (#152)
    introduces a real parser for Closure Compiler types, and uses the
    parser to interpret type expressions in JSDoc tags.
    
    TODO:
    - provide a way to override the type expression
    - update templateHelper to generate the correct links in type
    applications
    
    future enhancement (to be filed as a new issue): create pseudo-tags for
    members that are described in the type expression (e.g., if the type
    expression for the parameter `foo` is `{bar: string}`, add a tag for
    `foo.bar` with no description)

--- test/specs/tags/typetag.js
+++ test/specs/tags/typetag.js
@@ -6,7 +6,7 @@ describe("@type tag", function() {
     it('When a symbol has an @type tag, the doclet has a type property set to that value\'s type.', function() {
         expect(typeof foo.type).toBe('object');
         expect(typeof foo.type.names).toBe('object');
-        expect(foo.type.names.join(', ')).toBe('string, Array<string>');
+        expect(foo.type.names.join(', ')).toBe('string, Array.<string>');
     });
 
     it('When a symbol has an @type tag set to a plain string, the doclet has a type property set to that string as if it were a type.', function() {
