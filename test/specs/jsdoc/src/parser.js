eb9d95fa60e46309ad92312fad69311c5bc13625
Jeff Williams <jeffrey.l.williams@gmail.com>
2013-03-23 18:36:47 -0700

    add 'parseBegin' and 'parseComplete' events (#299)

--- test/specs/jsdoc/src/parser.js
+++ test/specs/jsdoc/src/parser.js
@@ -1,4 +1,4 @@
-/*global beforeEach: true, describe: true, expect: true, it: true, jasmine: true, xdescribe: true */
+/*global beforeEach: true, describe: true, expect: true, it: true, jasmine: true, xdescribe: true, xit: true */
 describe("jsdoc/src/parser", function() {
     var jsdoc = {src: { parser: require('jsdoc/src/parser')}};
 
@@ -32,9 +32,32 @@ describe("jsdoc/src/parser", function() {
         describe("parse", function() {
             beforeEach(newParser);
 
+            it("should fire 'parseBegin' events before it parses any files", function() {
+                var spy = jasmine.createSpy(),
+                    sourceFiles = ["javascript:/** @name foo */"];
+
+                parser.on("parseBegin", spy).parse(sourceFiles);
+                expect(spy).toHaveBeenCalled();
+                expect(spy.mostRecentCall.args[0].sourcefiles).toBe(sourceFiles);
+            });
+
+            it("should allow 'parseBegin' handlers to modify the list of source files", function() {
+                var sourceCode = "javascript:/** @name foo */",
+                    newFiles = ["[[replaced]]"],
+                    evt;
+
+                function handler(e) {
+                    e.sourcefiles = newFiles;
+                    evt = e;
+                }
+
+                parser.on('parseBegin', handler).parse(sourceCode);
+                expect(evt.sourcefiles).toBe(newFiles);
+            });
+
             it("should fire 'jsdocCommentFound' events when parsing source containing jsdoc comments", function() {
                 var spy = jasmine.createSpy(),
-                    sourceCode = 'javascript:/** @name bar */';
+                    sourceCode = ['javascript:/** @name bar */'];
                 parser.on('jsdocCommentFound', spy).parse(sourceCode);
                 expect(spy).toHaveBeenCalled();
                 expect(spy.mostRecentCall.args[0].comment).toEqual("/** @name bar */");
@@ -45,6 +68,14 @@ describe("jsdoc/src/parser", function() {
                     sourceCode = 'javascript:var foo = 1';
                 parser.on('symbolFound', spy).parse(sourceCode);
                 expect(spy).toHaveBeenCalled();
+            });
+
+            it("should fire 'parseComplete' events after it finishes parsing files", function() {
+                var spy = jasmine.createSpy(),
+                    sourceCode = ['javascript:var bar = false;'];
+                parser.on('parseComplete', spy).parse(sourceCode);
+                expect(spy).toHaveBeenCalled();
+                expect(spy.mostRecentCall.args[0].sourcefiles).toEqual(["[[string0]]"]);
             });
             
             it("should be able to parse its own source file", function() {
@@ -63,7 +94,13 @@ describe("jsdoc/src/parser", function() {
         describe("results", function() {
             beforeEach(newParser);
 
-            // TODO: more tests?
+            xit("contains an empty array before files are parsed", function() {
+                // TODO
+            });
+
+            xit("contains an array of doclets after files are parsed", function() {
+
+            });
 
             it("should reflect comment changes made by 'jsdocCommentFound' handlers", function() {
                 // we test both POSIX and Windows line endings
