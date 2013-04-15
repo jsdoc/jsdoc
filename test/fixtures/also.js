883b181ac747e318487d29c1b860ccd7ed308fd6
Jeff Williams <jeffrey.l.williams@gmail.com>
2013-04-14 17:50:25 -0700

    use the correct comment when creating doclets for overloaded methods (#179)

--- test/fixtures/also.js
+++ test/fixtures/also.js
@@ -1,6 +1,8 @@
 /** @class */
 function Asset() {
     this._name = '';
+    this._shape = '';
+    this._shhhhKeepThisSecret = '';
 }
 
 /**
@@ -17,4 +19,28 @@ function Asset() {
 Asset.prototype.name = function(newName) {
     if (newName) { this._name = newName; }
     else { return this._name; }
-}
+};
+
+/**
+ * Set the value of the shape property.
+ * @param {string} newShape
+ *//**
+ * Set the value of the shape property, plus some other property.
+ * @param {string} newShape
+ * @param {string} mysteryProperty
+ *//**
+ * Get the value of the shape property.
+ * @returns {string}
+ */
+Asset.prototype.shape = function(newShape, mysteryProperty) {
+    if (newShape && mysteryProperty) {
+        this._shape = newShape;
+        this._shhhhKeepThisSecret = mysteryProperty;
+    }
+    else if (newShape) {
+        this._shape = newShape;
+    }
+    else {
+        return this._shape;
+    }
+};
--- test/specs/documentation/also.js
+++ test/specs/documentation/also.js
@@ -1,10 +1,47 @@
+/*global describe: true, expect: true, it: true, jasmine: true */
 describe("multiple doclets per symbol", function() {
-    var docSet = jasmine.getDocSetFromFile('test/fixtures/also.js'),
-        name = docSet.getByLongname('Asset#name').filter(function($) {
-            return ! $.undocumented;
-        });
+    function undocumented($) {
+        return ! $.undocumented;
+    }
 
-    it('When a symbol has two doclets adjacent to each other both doclets apply to the symbol.', function() {
-        expect(name.length).toEqual(2);
+    function checkInequality(doclets, property) {
+        for (var l = doclets.length - 1; l > 0; l--) {
+            if (doclets[l][property] !== undefined && doclets[l - 1][property] !== undefined) {
+                expect(doclets[l][property]).not.toBe(doclets[l - 1][property]);
+            }
+        }
+    }
+
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/also.js');
+    var name = docSet.getByLongname('Asset#name').filter(undocumented);
+    var shape = docSet.getByLongname('Asset#shape').filter(undocumented);
+
+    it('When a symbol has multiple adjacent JSDoc comments, both apply to the symbol.', function() {
+        expect(name.length).toBe(2);
+        expect(shape.length).toBe(3);
+    });
+
+    it('When a symbol has multiple adjacent JSDoc comments that are not identical, the doclets ' +
+        'have different comments.', function() {
+        checkInequality(name, 'comment');
+        checkInequality(shape, 'comment');
+    });
+
+    it('When a symbol has multiple adjacent JSDoc comments with different descriptions, ' +
+        'the doclets have different descriptions.', function() {
+        checkInequality(name, 'description');
+        checkInequality(shape, 'description');
+    });
+
+    it('When a symbol has multiple adjacent JSDoc comments with different numbers of ' +
+        '@param tags, the doclets have different parameter lists.', function() {
+        checkInequality(name, 'params.length');
+        checkInequality(shape, 'params.length');
+    });
+
+    it('When a symbol has multiple adjacent JSDoc comments with different numbers of ' +
+        '@returns tags, the doclets have different lists of return values.', function() {
+        checkInequality(name, 'returns.length');
+        checkInequality(shape, 'returns.length');
     });
 });
