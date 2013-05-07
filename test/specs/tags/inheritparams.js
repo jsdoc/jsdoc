4d4786499c7f39ce81eac2d0433f4cdd775b9d7c
Ernst Haagsman <ernst@solpeo.pl>
2013-05-07 15:42:09 +0200

    @inheritparams tag: fixed bugs and improved unit test
    Deep clone of parameters ensures that original parameters aren't changed.
    Template now displays 'attributes' in parameters if they are only inherited
    and not also optional (etc). Fixed mistake in unit test, and expanded.
    Tests all green.

--- test/specs/tags/inheritparams.js
+++ test/specs/tags/inheritparams.js
@@ -4,18 +4,24 @@ describe("@inheritparams tag", function(){
         circle = docSet.getByLongname('Circle')[0];
 
     it('Circle should have inherited the \'config.fill\' parameter from Shape', function(){
-        shape_param = shape.params.filter(function(param){
-            if (param.name = 'config.fill') return true;
+        var shape_param = shape.params.filter(function(param){
+            if (param.name == 'config.fill') return true;
         })[0];
 
-        circle_param = circle.params.filter(function(param){
-            if (param.name = 'config.fill') return true;
+        var circle_param = circle.params.filter(function(param){
+            if (param.name == 'config.fill') return true;
         })[0];
 
+        expect(circle_param.inherited).toBe(true);
+        delete(circle_param.inherited);
         expect(shape_param).toEqual(circle_param);
     });
 
     it('Circle should have exactly 3 parameters', function(){
         expect(circle.params.length).toBe(3);
+    });
+
+    it('Shape should have exactly 2 parameters', function(){
+        expect(shape.params.length).toBe(2);
     });
 });
