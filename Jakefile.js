c59bc8cc92bc72a0e3b16620878a35cafa938d2a
Jeff Williams <jeffrey.l.williams@gmail.com>
2013-11-04 08:13:03 -0800

    bump version to 3.2.2

--- Jakefile.js
+++ Jakefile.js
@@ -15,7 +15,7 @@ task('default', [], function(params) {
 
     var metadata = {
         appname : 'jsdoc',
-        appversion : '3.2.1',
+        appversion : '3.2.2',
         timestamp : '' + new Date().getTime()
     };
 
