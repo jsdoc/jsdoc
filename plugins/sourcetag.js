c96a1215bd30fbb679edd21a4114922576fa7401
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-02-01 19:36:34 -0800

    add a warning about potential crashes (#554)

--- plugins/sourcetag.js
+++ plugins/sourcetag.js
@@ -11,6 +11,11 @@ exports.handlers = {
         Support @source tag. Expected value like:
             { "filename": "myfile.js", "lineno": 123 }
         Modifies the corresponding meta values on the given doclet.
+
+        WARNING: If you are using a JSDoc template that generates pretty-printed source files,
+        such as JSDoc's default template, this plugin can cause JSDoc to crash. To fix this issue,
+        update your template settings to disable pretty-printed source files.
+
         @source { "filename": "sourcetag.js", "lineno": 13 }
      */
     newDoclet: function(e) {
