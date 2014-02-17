56a4c89f99fbc2d4fb87df76c515341cc4fe7aa5
av.malutin <av.malutin@tensor.ru>
2014-02-17 16:06:15 +0400

    types parser test

---
+++ test/fixtures/type.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number=} Optional 
+ * @param {...number} Variable (repeatable)
+ * @param {*} All
+ * @param {?number} Nullable
+ * @param {!number} Non nullable
+ * @param {Array.<string>}
+ * @param {Object.<string, *>}
+ * @param {{a: string, b}} Record
+ * @param {Object.<string, {a, b}}>}
+ */
+function testFunction() {
+}
