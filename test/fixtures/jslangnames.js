65b2fd64998750ca1abbf2c7dae32d96bc9840b4
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-03-27 14:42:54 -0700

    fix typo; cleanup

--- test/fixtures/jslangnames.js
+++ test/fixtures/jslangnames.js
@@ -5,19 +5,19 @@ var constructor = {
 };
 
 /** @namespace */
-var prototye = {
+var prototype = {
     /** document me */
     valueOf: function(){}
-}
+};
 
 /**
-    This is Object
-    @namespace Object
-*/
+ * This is Object
+ * @namespace Object
+ */
 
 /**
-    This is Object.hasOwnProperty
-    @method Object.hasOwnProperty
+ * This is Object.hasOwnProperty
+ * @method Object.hasOwnProperty
  */
 
 // NOTE: you can't document a prototype of an object in JSDoc -- seriously, you just can't
