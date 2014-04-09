933db9f03238072f9d18f69159b096919f608321
jeffwi <jefesaurus@jefesaurus-macbookpro.roam.corp.google.com>
2014-04-09 16:50:47 -0700

    handle Esprima parse errors more gracefully (#625)

--- test/specs/jsdoc/src/astbuilder.js
+++ test/specs/jsdoc/src/astbuilder.js
@@ -1,4 +1,46 @@
-/*global describe: true, env: true, expect: true, it: true, xdescribe: true */
-xdescribe('jsdoc/src/astbuilder', function() {
-    // TODO
+/*global beforeEach, describe, expect, it, spyOn, xdescribe */
+describe('jsdoc/src/astbuilder', function() {
+    var astbuilder = require('jsdoc/src/astbuilder');
+
+    it('should exist', function() {
+        expect(astbuilder).toBeDefined();
+        expect(typeof astbuilder).toBe('object');
+    });
+
+    it('should export an AstBuilder class', function() {
+        expect(astbuilder.AstBuilder).toBeDefined();
+        expect(typeof astbuilder.AstBuilder).toBe('function');
+    });
+
+    describe('AstBuilder', function() {
+        // TODO: more tests
+        var builder;
+
+        beforeEach(function() {
+            builder = new astbuilder.AstBuilder();
+        });
+
+        it('should provide a "build" method', function() {
+            expect(builder.build).toBeDefined();
+            expect(typeof builder.build).toBe('function');
+        });
+
+        describe('build', function() {
+            // TODO: more tests
+            var logger = require('jsdoc/util/logger');
+
+            beforeEach(function() {
+                spyOn(logger, 'error');
+            });
+
+            it('should log (not throw) an error when a file cannot be parsed', function() {
+                function parse() {
+                    builder.build('if (true) { return; }', 'bad.js');
+                }
+
+                expect(parse).not.toThrow();
+                expect(logger.error).toHaveBeenCalled();
+            });
+        });
+    });
 });
