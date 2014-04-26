1566421a62b3c15bdaef14d38761de5c6648cb5e
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-04-26 10:57:10 -0700

    give doclets a longname even if the name is empty (#643)

---
+++ test/specs/documentation/classwithoutname.js
@@ -0,0 +1,14 @@
+/*global describe, expect, it, jasmine */
+describe('class without a name', function() {
+	var docSet = jasmine.getDocSetFromFile('test/fixtures/classwithoutname.js').doclets
+		.filter(function(doclet) {
+			return doclet.name === '';
+		});
+
+	it('When the doclet for a class has an empty name, it should also have an empty longname', function() {
+		expect(docSet).toBeDefined();
+		expect(docSet.length).toBe(1);
+		expect(docSet[0].description).toBe('Create an instance of MyClass.');
+		expect(docSet[0].longname).toBe('');
+	});
+});
