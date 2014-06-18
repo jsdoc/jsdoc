636bd83a172a9a7569a72b9416eaef8bbe7ed76b
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-06-17 21:34:48 -0700

    test cleanup

--- test/specs/jsdoc/doclet.js
+++ test/specs/jsdoc/doclet.js
@@ -1,18 +1,20 @@
-/*global describe: true, env: true, expect: true, it: true, jasmine: true */
-describe("jsdoc/doclet", function() {
+/*global describe, env, expect, it, jasmine */
+'use strict';
+
+describe('jsdoc/doclet', function() {
     // TODO: more tests
     var Doclet = require('jsdoc/doclet').Doclet;
 
-    var docSet = jasmine.getDocSetFromFile('test/fixtures/doclet.js'),
-        test1 = docSet.getByLongname('test1')[0],
-        test2 = docSet.getByLongname('test2')[0];
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/doclet.js');
+    var test1 = docSet.getByLongname('test1')[0];
+    var test2 = docSet.getByLongname('test2')[0];
 
-    var expectStrong = "**Strong** is strong";
-    var expectList = "* List item 1";
+    var expectList = '* List item 1';
+    var expectStrong = '**Strong** is strong';
 
     it('does not mangle Markdown in a description that uses leading asterisks', function() {
-        expect(test2.description.indexOf(expectStrong)).toBeGreaterThan(-1);
         expect(test2.description.indexOf(expectList)).toBeGreaterThan(-1);
+        expect(test2.description.indexOf(expectStrong)).toBeGreaterThan(-1);
     });
 
     describe('setScope', function() {
