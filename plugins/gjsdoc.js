f02bb97dc96bf3900c8cd8673d5a3416c388296b
MinHyeong Kim <romz1212@gmail.com>
2014-07-25 17:25:47 +0900

    add gjsdoc (customized jsdoc-toolkit annotation by Google) plugin.

---
+++ plugins/gjsdoc.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var _ = require('underscore');
+
+exports.handlers = {
+
+    /**
+     * replace google closure type annotation style (optional, defaultValue) to jsdoc3 annotation.
+     * "{boolean=} round [false] blah blah..." to "{boolean} [round=false]"
+     */
+    beforeParse: function(e) {
+        var source = e.source,
+            gjsStyleOptionalRegex = /\{(\(?[\w]+\)?)\=\}[\t\s]+(\w+)[\t\s]+\[([\w]+)\]/g,
+            match,
+
+            annotation,
+            type,
+            name,
+            defaultValue,
+
+            annotationLength,
+            index,
+
+            leftPart, rightPart;
+
+        while(match = gjsStyleOptionalRegex.exec(source)){
+            annotation = match[0];
+            type = match[1];
+            name = match[2];
+            defaultValue = match[3];
+
+            annotationLength = annotation.length;
+            index = match.index;
+
+            leftPart = source.substr(0, index);
+            rightPart = source.substr(index + annotationLength, source.length - (index + annotationLength));
+
+            e.source = source = leftPart + '{' + type + '}' + ' [' + name + '=' + defaultValue + ']' + rightPart;
+        }
+
+    }
+};
