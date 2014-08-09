c797b50c49a50b14775b2b3a1804ff90bcece80f
MinHyeong Kim <romz1212@gmail.com>
2014-08-09 21:35:13 +0900

    uncomment test code.

--- test/specs/tags/interfacetag.js
+++ test/specs/tags/interfacetag.js
@@ -1,10 +1,10 @@
 describe('@interface tag', function() {
     var docSet = jasmine.getDocSetFromFile("test/fixtures/interface-implements.js");
 
-    //var foundInterface = docSet.getByLongname('ITester');
+    var foundInterface = docSet.getByLongname('ITester');
 
-//    it('ITester has "interface" value in "kind"', function() {
-//        expect(foundInterface[0].kind).toBe('interface');
-//    });
+    it('ITester has "interface" value in "kind"', function() {
+        expect(foundInterface[0].kind).toBe('interface');
+    });
 
 });
