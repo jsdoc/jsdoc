89237260c661e040d47daf4c1957be62b8f2e6e5
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-08-12 09:37:00 -0700

    support virtual comments for overloaded symbols (#727)

--- test/specs/documentation/virtual.js
+++ test/specs/documentation/virtual.js
@@ -30,4 +30,24 @@ describe('virtual symbols', function() {
             expect(say.undocumented).not.toBeDefined();
         });
     });
+
+    describe('overloaded virtual symbols', function() {
+        var docSet = jasmine.getDocSetFromFile('test/fixtures/virtual3.js');
+        var constructors = docSet.getByLongname('module:connection');
+
+        it('should create multiple doclets for overloaded virtual symbols', function() {
+            expect(constructors).toBeDefined();
+            expect(constructors.length).toBe(2);
+        });
+
+        it('should use the correct signature for each virtual symbol', function() {
+            expect(constructors[0]).toBeDefined();
+            expect(constructors[0].params).toBeDefined();
+            expect(Array.isArray(constructors[0].params)).toBe(true);
+            expect(constructors[0].params[0].name).toBe('name');
+
+            expect(constructors[1]).toBeDefined();
+            expect(constructors[1].params).not.toBeDefined();
+        });
+    });
 });
