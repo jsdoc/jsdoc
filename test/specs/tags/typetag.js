17fb5be0644cecc97b7b51c7e27c0eda0481add7
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-08-13 21:53:11 -0700

    for Closure, allow the `type` tag to have a description (#732)

--- test/specs/tags/typetag.js
+++ test/specs/tags/typetag.js
@@ -1,4 +1,14 @@
-/*global describe, expect, it, jasmine */
+/*global afterEach, describe, expect, it, jasmine, spyOn */
+'use strict';
+
+var definitions = require('jsdoc/tag/dictionary/definitions');
+var dictionary = require('jsdoc/tag/dictionary');
+var Dictionary = dictionary.Dictionary;
+var doclet = require('jsdoc/doclet');
+var logger = require('jsdoc/util/logger');
+
+var originalDictionary = dictionary;
+
 describe('@type tag', function() {
     var docSet = jasmine.getDocSetFromFile('test/fixtures/typetag.js');
 
@@ -20,5 +30,48 @@ describe('@type tag', function() {
         var baz = docSet.getByLongname('baz')[0];
 
         expect(baz.nullable).toBe(false);
+    });
+
+    describe('JSDoc tags', function() {
+        afterEach(function() {
+            doclet._replaceDictionary(originalDictionary);
+        });
+
+        it('When JSDoc tags are enabled, the @type tag does not accept a description.', function() {
+            var dict = new Dictionary();
+            var typeDocs;
+
+            definitions.defineTags(dict, definitions.jsdocTags);
+            doclet._replaceDictionary(dict);
+            spyOn(logger, 'warn');
+
+            typeDocs = jasmine.getDocSetFromFile('test/fixtures/typetag2.js');
+
+            expect(logger.warn).toHaveBeenCalled();
+        });
+    });
+
+    describe('Closure tags', function() {
+        afterEach(function() {
+            doclet._replaceDictionary(originalDictionary);
+        });
+
+        it('When Closure tags are enabled, the @type tag accepts a description.', function() {
+            var dict = new Dictionary();
+            var stringOrNumber;
+            var typeDocs;
+
+            definitions.defineTags(dict, definitions.closureTags);
+            doclet._replaceDictionary(dict);
+            spyOn(logger, 'warn');
+
+            typeDocs = jasmine.getDocSetFromFile('test/fixtures/typetag2.js');
+            stringOrNumber = typeDocs.getByLongname('stringOrNumber')[0];
+
+            expect(logger.warn).not.toHaveBeenCalled();
+
+            expect(stringOrNumber).toBeDefined();
+            expect(stringOrNumber.description).toBe('A string or a number.');
+        });
     });
 });
