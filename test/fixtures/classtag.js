081bd896b34665383c416ad39490f25202679902
Brian Donovan <donovan@squareup.com>
2014-09-16 16:25:54 -0700

    Process ClassDeclaration nodes as class symbols.

--- test/fixtures/classtag.js
+++ test/fixtures/classtag.js
@@ -7,6 +7,12 @@ var Ticker = function() {
 };
 
 /**
+    Describe the Subscription class here.
+ */
+class Subscription {
+}
+
+/**
     Describe the NewsSource class here.
     @class NewsSource
  */
