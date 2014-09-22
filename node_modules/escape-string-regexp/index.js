b594458c4d427a02d30c46563caea5a7d4a2ceb1
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-09-22 15:48:18 -0700

    update committed dependencies

--- node_modules/escape-string-regexp/index.js
+++ node_modules/escape-string-regexp/index.js
@@ -1,8 +1,11 @@
 'use strict';
+
+var matchOperatorsRe = /[|\\{}()[\]^$+*?.]/g;
+
 module.exports = function (str) {
 	if (typeof str !== 'string') {
 		throw new TypeError('Expected a string');
 	}
 
-	return str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+	return str.replace(matchOperatorsRe,  '\\$&');
 };
