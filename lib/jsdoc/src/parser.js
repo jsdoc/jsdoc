jsdoc/jsdoc

commit 2eb3c467eefefdac6796860e7896f50d8cae34b9

handle enums that are part of a chain of assignments (#702)
We now add the enum properties to all of the symbols in the assignment chain, rather than just the first symbol.

lib/jsdoc/src/parser.js
@@ -429,21 +429,48 @@ Parser.prototype.resolveThis = function(node) {
     return result;
 };
 
-// TODO: docs
 /**
- * Given 'var foo = { x: 1 }', find foo from x.
+ * Given an AST node representing an object property, find the doclets for the parent object or
+ * objects.
+ *
+ * If the object is part of a simple assignment (for example, `var foo = { x: 1 }`), this method
+ * returns a single doclet (in this case, the doclet for `foo`).
+ *
+ * If the object is part of a chained assignment (for example, `var foo = exports.FOO = { x: 1 }`,
+ * this method returns multiple doclets (in this case, the doclets for `foo` and `exports.FOO`).
+ *
+ * @param {Object} node - An AST node representing an object property.
+ * @return {Array.<jsdoc/doclet.Doclet>} An array of doclets for the parent object or objects, or
+ * an empty array if no doclets are found.
  */
-Parser.prototype.resolvePropertyParent = function(node) {
+Parser.prototype.resolvePropertyParents = function(node) {
+    var currentNode = node;
     var doclet;
+    var doclets = [];
 
-    if (node.parent) {
-        doclet = this._getDoclet(node.parent.nodeId);
-    }
+    do {
+        if (currentNode.parent) {
+            doclet = this._getDoclet(currentNode.parent.nodeId);
+            if (doclet) {
+                doclets.push(doclet);
+            }
 
-    return doclet;
+            // if the parent is an assignment expression (for example, `exports.FOO` in
+            // `var foo = exports.FOO = { x: 1 }`, keep walking upwards
+            if (currentNode.parent.parent.type === Syntax.AssignmentExpression) {
+                currentNode = currentNode.parent;
+            }
+            // otherwise, we're done
+            else {
+                currentNode = null;
+            }
+        }
+    } while (currentNode);
+
+    return doclets;
 };
 
-// TODO docs
+// TODO: docs
 /**
  * Resolve what function a var is limited to.
  * @param {astnode} node
@@ -477,24 +504,24 @@ Parser.prototype.resolveVar = function(node, basename) {
 
 // TODO: docs
 Parser.prototype.resolveEnum = function(e) {
-    var doclet = this.resolvePropertyParent(e.code.node.parent);
+    var doclets = this.resolvePropertyParents(e.code.node.parent);
 
-    if (doclet && doclet.isEnum) {
-        if (!doclet.properties) {
-            doclet.properties = [];
+    doclets.forEach(function(doclet) {
+        if (doclet && doclet.isEnum) {
+            doclet.properties = doclet.properties || [];
+
+            // members of an enum inherit the enum's type
+            if (doclet.type && !e.doclet.type) {
+                e.doclet.type = doclet.type;
+            }
+
+            delete e.doclet.undocumented;
+            e.doclet.defaultvalue = e.doclet.meta.code.value;
+
+            // add a copy of the doclet to the parent's properties
+            doclet.properties.push( jsdoc.util.doop(e.doclet) );
         }
-
-        // members of an enum inherit the enum's type
-        if (doclet.type && !e.doclet.type) {
-            e.doclet.type = doclet.type;
-        }
-
-        delete e.doclet.undocumented;
-        e.doclet.defaultvalue = e.doclet.meta.code.value;
-
-        // add a copy of the doclet to the parent's properties
-        doclet.properties.push( jsdoc.util.doop(e.doclet) );
-    }
+    });
 };
 
 // TODO: document other events

test/fixtures/enumtag2.js
@@ -1,0 +1,32 @@
+/** @module my/enums */
+
+/**
+ * Enum for quad-state values.
+ * @enum {number}
+ * @memberof module:my/enums
+ */
+var QuadState = exports.QuadState = {
+    /** true */
+    TRUE: 1,
+    FALSE: -1,
+    MAYBE: 0,
+    UNKNOWN: -99
+};
+
+/**
+ * Enum for penta-state values.
+ * @enum {number}
+ */
+exports.PentaState =
+/**
+ * Enum for penta-state values, BUT SHOUTIER.
+ * @enum {number}
+ */
+exports.PENTASTATE = {
+    /** true */
+    TRUE: 1,
+    FALSE: -1,
+    MAYBE: 0,
+    UNKNOWN: -99,
+    DONTCARE: 99
+};

test/specs/tags/enumtag.js
@@ -31,4 +31,28 @@ describe('@enum tag', function() {
 
         expect( dump(tristate) ).not.toMatch('<CircularRef>');
     });
+
+    describe('chained assignments', function() {
+        var pentaState;
+        var PENTASTATE;
+        var quadState;
+
+        docSet = jasmine.getDocSetFromFile('test/fixtures/enumtag2.js');
+        pentaState = docSet.getByLongname('module:my/enums.PentaState')[0];
+        PENTASTATE = docSet.getByLongname('module:my/enums.PENTASTATE')[0];
+        quadState = docSet.getByLongname('module:my/enums.QuadState')[0];
+
+        it('When a symbol at the start of an assignment chain has an @enum tag, that symbol has a properties array.', function() {
+            expect( Array.isArray(quadState.properties) ).toBe(true);
+            expect(quadState.properties.length).toBe(4);
+        });
+
+        it('When multiple symbols in an assignment chain have @enum tags, each symbol has a properties array.', function() {
+            expect( Array.isArray(pentaState.properties) ).toBe(true);
+            expect(pentaState.properties.length).toBe(5);
+
+            expect( Array.isArray(PENTASTATE.properties) ).toBe(true);
+            expect(pentaState.properties.length).toBe(5);
+        });
+    });
 });

commit c510b336d213cd51bc87637d6ccdd1c5ad565955

fix performance regression caused by 2eb3c46 (#784)

lib/jsdoc/src/parser.js
@@ -455,7 +455,7 @@ Parser.prototype.resolvePropertyParents = function(node) {
             doclets.push(doclet);
         }
 
-        // if the next ancestor's parent is an assignment expression (for example, `exports.FOO` in
+        // if the next ancestor is an assignment expression (for example, `exports.FOO` in
         // `var foo = exports.FOO = { x: 1 }`, keep walking upwards
         if (nextAncestor && nextAncestor.type === Syntax.AssignmentExpression) {
             nextAncestor = nextAncestor.parent;
@@ -512,14 +512,15 @@ Parser.prototype.resolveEnum = function(e) {
 
             // members of an enum inherit the enum's type
             if (doclet.type && !e.doclet.type) {
-                e.doclet.type = doclet.type;
+                // clone the type to prevent circular refs
+                e.doclet.type = jsdoc.util.doop(doclet.type);
             }
 
             delete e.doclet.undocumented;
             e.doclet.defaultvalue = e.doclet.meta.code.value;
 
-            // add a copy of the doclet to the parent's properties
-            doclet.properties.push( jsdoc.util.doop(e.doclet) );
+            // add the doclet to the parent's properties
+            doclet.properties.push(e.doclet);
         }
     });
 };