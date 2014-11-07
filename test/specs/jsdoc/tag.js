d17ecfd8ec6f4eb188fdb79bc8dfb3d5f8c53520
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-11-06 20:28:34 -0800

    remove cruft from Tag objects

--- test/specs/jsdoc/tag.js
+++ test/specs/jsdoc/tag.js
@@ -1,4 +1,3 @@
-/*global afterEach, beforeEach, describe, env, expect, it, spyOn */
 'use strict';
 
 var hasOwnProp = Object.prototype.hasOwnProperty;
@@ -43,6 +42,7 @@ describe('jsdoc/tag', function() {
         var tagExample;
         var tagExampleIndented;
         var tagParam;
+        var tagParamWithType;
         var tagType;
 
         // allow each test to recreate the tags (for example, after enabling debug mode)
@@ -51,8 +51,10 @@ describe('jsdoc/tag', function() {
             tagArg = new jsdoc.tag.Tag('arg  ', text, meta);
             // @param with no type, but with optional and defaultvalue
             tagParam = new jsdoc.tag.Tag('param', '[foo=1]', meta);
+            // @param with type and no type modifiers (such as optional)
+            tagParamWithType = new jsdoc.tag.Tag('param', '{string} foo', meta);
             // @example that does not need indentation to be removed
-            tagExample  = new jsdoc.tag.Tag('example', textExample, meta);
+            tagExample = new jsdoc.tag.Tag('example', textExample, meta);
             // @example that needs indentation to be removed
             tagExampleIndented = new jsdoc.tag.Tag('example', textExampleIndented, meta);
             // for testing that onTagText is run when necessary
@@ -204,6 +206,12 @@ describe('jsdoc/tag', function() {
                 expect(tagArg.value.name).toBe('foo');
 
                 expect(tagType.value.name).not.toBeDefined();
+            });
+
+            it('if the tag has a type without modifiers, tag.value should not include properties for the modifiers', function() {
+                ['optional', 'nullable', 'variable', 'defaultvalue'].forEach(function(modifier) {
+                    expect( hasOwnProp.call(tagParamWithType.value, modifier) ).toBe(false);
+                });
             });
         });
 
