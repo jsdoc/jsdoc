b22200760be2a4f605d67c5ae576a4df07c7a217
Alexandrine Boissiere <theasta@gmail.com>
2014-11-11 23:16:02 -0800

    Create a view manager class

--- templates/default/publish.js
+++ templates/default/publish.js
@@ -8,19 +8,15 @@ var docletHelper = require('jsdoc/util/docletHelper');
 var logger = require('jsdoc/util/logger');
 var path = require('jsdoc/path');
 var _ = require('underscore'); // should replace it with lodash
-var template = require('jsdoc/template');
 var util = require('util');
 
-var htmlsafe = helper.htmlsafe;
 var linkto = helper.linkto;
-var resolveAuthorLinks = helper.resolveAuthorLinks;
-var scopeToPunc = helper.scopeToPunc;
 var hasOwnProp = Object.prototype.hasOwnProperty;
+
+var View = require('jsdoc/util/view');
 
 var data;
 var view;
-
-var outdir = path.normalize(env.opts.destination);
 
 function find(spec) {
     return helper.find(data, spec);
@@ -34,58 +30,6 @@ function getAncestorLinks(doclet) {
     return helper.getAncestorLinks(data, doclet);
 }
 
-
-function shortenPaths(files, commonPrefix) {
-    Object.keys(files).forEach(function(file) {
-        files[file].shortened = files[file].resolved.replace(commonPrefix, '')
-            // always use forward slashes
-            .replace(/\\/g, '/');
-    });
-
-    return files;
-}
-
-
-function generate(title, docs, filename, resolveLinks) {
-    resolveLinks = resolveLinks === false ? false : true;
-
-    var docData = {
-        title: title,
-        docs: docs
-    };
-
-    var outpath = path.join(outdir, filename),
-        html = view.render('container.tmpl', docData);
-
-    if (resolveLinks) {
-        html = helper.resolveLinks(html); // turn {@link foo} into <a href="foodoc.html">foo</a>
-    }
-
-    fs.writeFileSync(outpath, html, 'utf8');
-}
-
-function generateSourceFiles(sourceFiles, encoding) {
-    encoding = encoding || 'utf8';
-    Object.keys(sourceFiles).forEach(function(file) {
-        var source;
-        // links are keyed to the shortened path in each doclet's `meta.shortpath` property
-        var sourceOutfile = helper.getUniqueFilename(sourceFiles[file].shortened);
-        helper.registerLink(sourceFiles[file].shortened, sourceOutfile);
-
-        try {
-            source = {
-                kind: 'source',
-                code: helper.htmlsafe( fs.readFileSync(sourceFiles[file].resolved, encoding) )
-            };
-        }
-        catch(e) {
-            logger.error('Error while generating source file %s: %s', file, e.message);
-        }
-
-        generate('Source: ' + sourceFiles[file].shortened, [source], sourceOutfile,
-            false);
-    });
-}
 
 /**
  * Look for classes or functions with the same name as modules (which indicates that the module
@@ -261,44 +205,18 @@ function buildNav(members) {
     @param {Tutorial} tutorials
  */
 exports.publish = function(taffyData, opts, tutorials) {
-    data = taffyData;
 
     var conf = env.conf.templates || {};
-    var defaultConfig = {
-        layoutFile: 'layout.tmpl',
-        staticFiles: false,
-        outputSourceFiles: true
-    };
-    var config = _.extend({}, conf['default'] || {}, defaultConfig);
-
-    var templatePath = path.normalize(opts.template);
-    view = new template.Template( path.join(templatePath, 'tmpl') );
-
-    // claim some special filenames in advance, so the All-Powerful Overseer of Filename Uniqueness
-    // doesn't try to hand them out later
-    var indexUrl = helper.getUniqueFilename('index');
-    // don't call registerLink() on this one! 'index' is also a valid longname
-
-    var globalUrl = helper.getUniqueFilename('global');
-    helper.registerLink('global', globalUrl);
-
-    // set up templating
-    view.layout = (config.layoutFile !== defaultConfig.layoutFile) ?
-        path.getResourcePath(path.dirname(config.layoutFile), path.basename(config.layoutFile)) :
-        config.layoutFile;
-
+    var viewManager = new View(taffyData, opts, conf.default || {});
+    view = viewManager.view;
 
     // set up tutorials for helper
     helper.setTutorials(tutorials);
 
-    data = helper.prune(data);
-    data.sort('longname, version, since');
-    helper.addEventListeners(data);
+    data = viewManager.data;
 
-    var sourceFiles = {};
-    var sourceFilePaths = [];
     data().each(function(doclet) {
-         doclet.attribs = '';
+        doclet.attribs = '';
 
         if (doclet.examples) {
             doclet.examples = doclet.examples.map(function(example) {
@@ -320,78 +238,16 @@ exports.publish = function(taffyData, opts, tutorials) {
                 doclet.see[i] = docletHelper.hashToLink(doclet, seeItem);
             });
         }
-
-        // build a list of source files
-        var sourcePath;
-        if (doclet.meta) {
-            sourcePath = docletHelper.getPathFromDoclet(doclet);
-            sourceFiles[sourcePath] = {
-                resolved: sourcePath,
-                shortened: null
-            };
-            if (sourceFilePaths.indexOf(sourcePath) === -1) {
-                sourceFilePaths.push(sourcePath);
-            }
-        }
     });
 
-    // update outdir if necessary, then create outdir
-    var packageInfo = ( find({kind: 'package'}) || [] ) [0];
-    if (packageInfo && packageInfo.name) {
-        outdir = path.join( outdir, packageInfo.name, (packageInfo.version || '') );
-    }
-    fs.mkPath(outdir);
+    viewManager.processSourceFilesStepOne();
 
-    // copy the template's static files to outdir
-    var fromDir = path.join(templatePath, 'static');
-    var staticFiles = fs.ls(fromDir, 3);
-
-    staticFiles.forEach(function(fileName) {
-        var toDir = fs.toDir( fileName.replace(fromDir, outdir) );
-        fs.mkPath(toDir);
-        fs.copyFileSync(fileName, toDir);
-    });
-
-    // copy user-specified static files to outdir
-    var staticFilePaths;
-    var staticFileFilter;
-    var staticFileScanner;
-    if (config.staticFiles) {
-        // The canonical property name is `include`. We accept `paths` for backwards compatibility
-        // with a bug in JSDoc 3.2.x.
-        staticFilePaths = config.staticFiles.include || config.staticFiles.paths || [];
-        staticFileFilter = new (require('jsdoc/src/filter')).Filter(config.staticFiles);
-        staticFileScanner = new (require('jsdoc/src/scanner')).Scanner();
-
-        staticFilePaths.forEach(function(filePath) {
-            var extraStaticFiles = staticFileScanner.scan([filePath], 10, staticFileFilter);
-
-            extraStaticFiles.forEach(function(fileName) {
-                var sourcePath = fs.toDir(filePath);
-                var toDir = fs.toDir( fileName.replace(sourcePath, outdir) );
-                fs.mkPath(toDir);
-                fs.copyFileSync(fileName, toDir);
-            });
-        });
-    }
-
-    if (sourceFilePaths.length) {
-        sourceFiles = shortenPaths( sourceFiles, path.commonPrefix(sourceFilePaths) );
-    }
     data().each(function(doclet) {
         var url = helper.createLink(doclet);
         helper.registerLink(doclet.longname, url);
-
-        // add a shortened version of the full path
-        var docletPath;
-        if (doclet.meta) {
-            docletPath = docletHelper.getPathFromDoclet(doclet);
-            docletPath = sourceFiles[docletPath].shortened;
-            if (docletPath) {
-                doclet.meta.shortpath = docletPath;
-            }
-        }
     });
+
+    viewManager.processSourceFilesStepTwo();
 
     data().each(function(doclet) {
         var url = helper.longnameToUrl[doclet.longname];
@@ -429,92 +285,60 @@ exports.publish = function(taffyData, opts, tutorials) {
     var members = helper.getMembers(data);
     members.tutorials = tutorials.children;
 
-    // output pretty-printed source files by default
-    var outputSourceFiles = config.outputSourceFiles !== false;
-
     // add template helpers
     view.find = find;
-    view.linkto = linkto;
-    view.resolveAuthorLinks = resolveAuthorLinks;
     view.tutoriallink = tutoriallink;
-    view.htmlsafe = htmlsafe;
-    view.outputSourceFiles = outputSourceFiles;
 
     // once for all
     view.nav = buildNav(members);
     attachModuleSymbols( find({ longname: {left: 'module:'} }), members.modules );
 
     // generate the pretty-printed source files first so other pages can link to them
-    if (outputSourceFiles) {
-        generateSourceFiles(sourceFiles, opts.encoding);
+    viewManager.generateSourceFiles();
+
+    if (members.globals.length) {
+        viewManager.generateGlobalPage('Global');
     }
 
-    if (members.globals.length) { generate('Global', [{kind: 'globalobj'}], globalUrl); }
-
-    // index page displays information from package.json and lists files
-    var files = find({kind: 'file'}),
-        packages = find({kind: 'package'});
-
-    generate('Home',
-        packages.concat(
-            [{kind: 'mainpage', readme: opts.readme, longname: (opts.mainpagetitle) ? opts.mainpagetitle : 'Main Page'}]
-        ).concat(files),
-    indexUrl);
+    viewManager.generateMainPage('Home');
 
     Object.keys(helper.longnameToUrl).forEach(function(longname) {
         var myClasses = _.where(members.classes, {longname: longname});
         if (myClasses.length) {
-            generate('Class: ' + myClasses[0].name, myClasses, helper.longnameToUrl[longname]);
+            viewManager.generate('Class: ' + myClasses[0].name, myClasses, helper.longnameToUrl[longname]);
         }
 
         var myModules = _.where(members.modules, {longname: longname});
         if (myModules.length) {
-            generate('Module: ' + myModules[0].name, myModules, helper.longnameToUrl[longname]);
+            viewManager.generate('Module: ' + myModules[0].name, myModules, helper.longnameToUrl[longname]);
         }
 
         var myNamespaces = _.where(members.namespaces, {longname: longname});
         if (myNamespaces.length) {
-            generate('Namespace: ' + myNamespaces[0].name, myNamespaces, helper.longnameToUrl[longname]);
+            viewManager.generate('Namespace: ' + myNamespaces[0].name, myNamespaces, helper.longnameToUrl[longname]);
         }
 
         var myMixins = _.where(members.mixins, {longname: longname});
         if (myMixins.length) {
-            generate('Mixin: ' + myMixins[0].name, myMixins, helper.longnameToUrl[longname]);
+            viewManager.generate('Mixin: ' + myMixins[0].name, myMixins, helper.longnameToUrl[longname]);
         }
 
         var myExternals = _.where(members.externals, {longname: longname});
         if (myExternals.length) {
-            generate('External: ' + myExternals[0].name, myExternals, helper.longnameToUrl[longname]);
+            viewManager.generate('External: ' + myExternals[0].name, myExternals, helper.longnameToUrl[longname]);
         }
 
         var myInterfaces = _.where(members.interfaces, {longname: longname});
         if (myInterfaces.length) {
-            generate('Interface: ' + myInterfaces[0].name, myInterfaces, helper.longnameToUrl[longname]);
+            viewManager.generate('Interface: ' + myInterfaces[0].name, myInterfaces, helper.longnameToUrl[longname]);
         }
     });
 
-    // TODO: move the tutorial functions to templateHelper.js
-    function generateTutorial(title, tutorial, filename) {
-        var tutorialData = {
-            title: title,
-            header: tutorial.title,
-            content: tutorial.parse(),
-            children: tutorial.children
-        };
-
-        var tutorialPath = path.join(outdir, filename),
-            html = view.render('tutorial.tmpl', tutorialData);
-
-        // yes, you can use {@link} in tutorials too!
-        html = helper.resolveLinks(html); // turn {@link foo} into <a href="foodoc.html">foo</a>
-
-        fs.writeFileSync(tutorialPath, html, 'utf8');
-    }
 
     // tutorials can have only one parent so there is no risk for loops
     function saveChildren(node) {
         node.children.forEach(function(child) {
-            generateTutorial('Tutorial: ' + child.title, child, helper.tutorialToUrl(child.name));
+            viewManager.generateTutorial('Tutorial: ' + child.title, child, helper.tutorialToUrl(child.name));
             saveChildren(child);
         });
     }
