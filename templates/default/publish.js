27d7ec3f756f1ff9a33274cfe9cd3891c55891ac
Alexandrine Boissiere <theasta@gmail.com>
2014-11-15 14:59:19 -0800

    Generate pages from the constructor

--- templates/default/publish.js
+++ templates/default/publish.js
@@ -1,7 +1,7 @@
 /*global env: true */
 'use strict';
 
-var View = require('jsdoc/util/view');
+var TemplateRenderer = require('jsdoc/util/view');
 
 /**
     @param {TAFFY} taffyData See <http://taffydb.com/>.
@@ -9,18 +9,6 @@ var View = require('jsdoc/util/view');
     @param {Tutorial} tutorials
  */
 exports.publish = function(taffyData, opts, tutorials) {
-
     var conf = env.conf.templates || {};
-    var viewManager = new View(taffyData, tutorials, opts, conf.default || {});
-
-    // generate the pretty-printed source files first so other pages can link to them
-    viewManager.generateSourceFiles();
-
-    viewManager.generateGlobalPage('Global');
-
-    viewManager.generateMainPage('Home');
-
-    viewManager.generatePages('Home');
-
-    viewManager.generateTutorials();
+    new TemplateRenderer(taffyData, tutorials, opts, conf.default || {});
 };
