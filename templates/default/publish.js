d29ab8f7721a8765bee2a55ab0de71f58cacbb59
Alexandrine Boissiere <theasta@gmail.com>
2014-11-16 13:02:33 -0800

    Move templateOptions logic to the TemplateRenderer class

--- templates/default/publish.js
+++ templates/default/publish.js
@@ -4,12 +4,21 @@
 
 var TemplateRenderer = require('jsdoc/util/templateRenderer');
 
+var _ = require('underscore');
+var helper = require('jsdoc/util/templateHelper');
+var path = require('jsdoc/path');
+var fs = require('jsdoc/fs');
+var template = require('jsdoc/template');
+var docletHelper = require('jsdoc/util/docletHelper');
+var logger = require('jsdoc/util/logger');
+var doop = require('jsdoc/util/doop');
+var util = require('util');
+
 /**
     @param {TAFFY} taffyData See <http://taffydb.com/>.
     @param {object} opts
     @param {Tutorial} tutorials
  */
 exports.publish = function(taffyData, opts, tutorials) {
-    var conf = env.conf.templates || {};
-    new TemplateRenderer(taffyData, tutorials, opts, conf.default || {});
+    new TemplateRenderer(taffyData, tutorials, opts);
 };
