f1354563965792b8dd183f5a5331f8fcc67ea319
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-11-27 11:48:11 -0800

    don't overwrite the `classdesc` tag's value when the `class` and `constructor` tags are also present (#806)

--- test/specs/tags/classdesctag.js
+++ test/specs/tags/classdesctag.js
@@ -1,8 +1,15 @@
-describe("@classdesc tag", function() {
-    var docSet = jasmine.getDocSetFromFile('test/fixtures/classdesctag.js'),
-        doc = docSet.getByLongname('Foo')[0];
+'use strict';
 
-    it('adds a classdesc property to the doclet with the description', function() {
-        expect(doc.classdesc).toBe('A description of the class.');
+describe('@classdesc tag', function() {
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/classdesctag.js');
+    var foo = docSet.getByLongname('Foo')[0];
+    var bar = docSet.getByLongname('Bar')[0];
+
+    it('should add a classdesc property to the doclet with the description', function() {
+        expect(foo.classdesc).toBe('A description of the class.');
+    });
+
+    it('should work when the @class and @constructor tags are also present, and @class has a value', function() {
+        expect(bar.classdesc).toBe('A description of the class.');
     });
 });
