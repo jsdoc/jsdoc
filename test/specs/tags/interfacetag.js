869f63113ff3c4086804de2919c8ac84e17c21a6
Jeff Williams <jeffrey.l.williams@gmail.com>
2014-12-18 16:41:51 -0800

    allow `interface` tag to have a name in the JSDoc tag dictionary (#828)

--- test/specs/tags/interfacetag.js
+++ test/specs/tags/interfacetag.js
@@ -1,8 +1,12 @@
 'use strict';
 
 describe('@interface tag', function() {
-    var docSet = jasmine.getDocSetFromFile('test/fixtures/interface-implements.js');
+    var definitions = require('jsdoc/tag/dictionary/definitions');
+    var Dictionary = require('jsdoc/tag/dictionary').Dictionary;
+    var doclet = require('jsdoc/doclet');
+    var logger = require('jsdoc/util/logger');
 
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/interface-implements.js');
     var testerInterface = docSet.getByLongname('ITester')[0];
     var testerImplementation = docSet.getByLongname('MyTester')[0];
 
@@ -12,5 +16,50 @@ describe('@interface tag', function() {
 
     it('MyTester class has its kind set to "class" (not "interface")', function() {
         expect(testerImplementation.kind).toBe('class');
+    });
+
+    describe('virtual doclets', function() {
+        var tagDict;
+
+        beforeEach(function() {
+            spyOn(logger, 'warn');
+        });
+
+        afterEach(function() {
+            tagDict = new Dictionary();
+            definitions.defineTags(tagDict);
+            doclet._replaceDictionary(tagDict);
+        });
+
+        it('should support virtual doclets with the JSDoc tag dictionary', function() {
+            var docSet2;
+            var virtualInterface;
+
+            tagDict = new Dictionary();
+            definitions.defineTags(tagDict, definitions.jsdocTags);
+            doclet._replaceDictionary(tagDict);
+
+            docSet2 = jasmine.getDocSetFromFile('test/fixtures/interfacetag2.js');
+            virtualInterface = docSet2.getByLongname('VirtualInterface')[0];
+
+            expect(logger.warn).not.toHaveBeenCalled();
+            expect(virtualInterface).toBeDefined();
+            expect(virtualInterface.longname).toBe('VirtualInterface');
+        });
+
+        it('should not support virtual doclets with the Closure tag dictionary', function() {
+            var docSet2;
+            var virtualInterface;
+
+            tagDict = new Dictionary();
+            definitions.defineTags(tagDict, definitions.closureTags);
+            doclet._replaceDictionary(tagDict);
+
+            docSet2 = jasmine.getDocSetFromFile('test/fixtures/interfacetag2.js');
+            virtualInterface = docSet2.getByLongname('VirtualInterface')[0];
+
+            expect(logger.warn).toHaveBeenCalled();
+            expect(virtualInterface).not.toBeDefined();
+        });
     });
 });
