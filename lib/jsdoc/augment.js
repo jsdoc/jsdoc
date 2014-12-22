jsdoc/jsdoc

commit 39e5008cfc84fe3f88d6e6e75be15241bbd671fe

implement `inheritdoc` and `override` tags (#53)

lib/jsdoc/augment.js
@@ -15,15 +15,17 @@ function mapDependencies(index, propertyName) {
     var doc;
     var doclets;
     var kinds = ['class', 'external', 'mixin'];
-    var len;
+    var len = 0;
 
     Object.keys(index).forEach(function(name) {
         doclets = index[name];
-        for (var i = 0, ii = doclets.length; i < ii; ++i) {
+        for (var i = 0, ii = doclets.length; i < ii; i++) {
             doc = doclets[i];
             if (kinds.indexOf(doc.kind) !== -1) {
                 dependencies[name] = {};
-                len = doc[propertyName] && doc[propertyName].length || 0;
+                if (hasOwnProp.call(doc, propertyName)) {
+                    len = doc[propertyName].length;
+                }
                 for (var j = 0; j < len; ++j) {
                     dependencies[name][doc[propertyName][j]] = true;
                 }
@@ -87,9 +89,9 @@ function getMembers(longname, docs, scopes) {
     return members;
 }
 
-function addOverridesProperty(doclets, overrides) {
+function addDocletProperty(doclets, propName, value) {
     for (var i = 0, l = doclets.length; i < l; i++) {
-        doclets[i].overrides = overrides;
+        doclets[i][propName] = value;
     }
 }
 
@@ -142,6 +144,21 @@ function updateAddedDoclets(doclet, additions, indexes) {
     }
 }
 
+function explicitlyInherits(doclets) {
+    var doclet;
+    var inherits = false;
+
+    for (var i = 0, l = doclets.length; i < l; i++) {
+        doclet = doclets[i];
+        if (typeof doclet.inheritdoc !== 'undefined' || typeof doclet.override !== 'undefined') {
+            inherits = true;
+            break;
+        }
+    }
+
+    return inherits;
+}
+
 function getInheritedAdditions(doclets, docs, documented) {
     var additionIndexes;
     var additions = [];
@@ -173,13 +190,6 @@ function getInheritedAdditions(doclets, docs, documented) {
                     }
                     member.inherited = true;
 
-                    // Remove the `overrides` property if present. (For classes A > B > C, if B#a
-                    // overrides A#a, and C#a inherits B#a, we don't want the doclet for C#a to say
-                    // that it overrides A#a.)
-                    if (member.overrides) {
-                        delete member.overrides;
-                    }
-
                     // TODO: this will fail on longnames like: MyClass#"quoted#Longname"
                     // and nested instance members like: MyClass#MyOtherClass#myMethod;
                     // switch to updateLongname()!
@@ -188,15 +198,42 @@ function getInheritedAdditions(doclets, docs, documented) {
                     parts[0] = doc.longname;
                     member.longname = parts.join('#');
 
-                    // Add the ancestor's docs, unless the descendant overrides the ancestor AND
+                    // Indicate what the descendant is overriding. (We only care about the closest
+                    // ancestor. For classes A > B > C, if B#a overrides A#a, and C#a inherits B#a,
+                    // we don't want the doclet for C#a to say that it overrides A#a.)
+                    addDocletProperty([member], 'overrides', members[k].longname);
+
+                    // Add the ancestor's docs unless the descendant overrides the ancestor AND
                     // documents the override.
                     if ( !hasOwnProp.call(documented, member.longname) ) {
                         updateAddedDoclets(member, additions, additionIndexes);
                     }
-                    // If the descendant is documented and overrides an ancestor, indicate what the
-                    // descendant is overriding.
+                    // If the descendant used an @inheritdoc or @override tag, add the ancestor's
+                    // docs, and ignore the existing doclets.
+                    else if ( explicitlyInherits(documented[member.longname]) ) {
+                        // Ignore any existing doclets. (This is safe because we only get here if
+                        // `member.longname` is an own property of `documented`.)
+                        addDocletProperty(documented[member.longname], 'ignore', true);
+
+                        updateAddedDoclets(member, additions, additionIndexes);
+
+                        // Remove property that's no longer accurate.
+                        if (member.virtual) {
+                            delete member.virtual;
+                        }
+                        // Remove properties that we no longer need.
+                        if (member.inheritdoc) {
+                            delete member.inheritdoc;
+                        }
+                        if (member.override) {
+                            delete member.override;
+                        }
+                    }
+                    // If the descendant overrides the ancestor and documents the override,
+                    // update the doclets to indicate what the descendant is overriding.
                     else {
-                        addOverridesProperty(documented[member.longname], members[k].longname);
+                        addDocletProperty(documented[member.longname], 'overrides',
+                            members[k].longname);
                     }
                 }
             }

lib/jsdoc/schema.js
@@ -348,6 +348,10 @@ var DOCLET_SCHEMA = exports.DOCLET_SCHEMA = {
                 type: STRING
             }
         },
+        inheritdoc: {
+            type: STRING,
+            optional: true
+        },
         inherited: {
             type: BOOLEAN,
             optional: true
@@ -431,6 +435,11 @@ var DOCLET_SCHEMA = exports.DOCLET_SCHEMA = {
         // is this member optional? (derived from the type expression)
         optional: {
             type: BOOLEAN_OPTIONAL
+        },
+        // does this member explicitly override the parent?
+        override: {
+            type: BOOLEAN,
+            optional: true
         },
         overrides: {
             type: STRING,

lib/jsdoc/tag/dictionary/definitions.js
@@ -456,6 +456,13 @@ var baseTags = exports.baseTags = {
             doclet.implements.push(tag.value);
         }
     },
+    inheritdoc: {
+        mustNotHaveValue: true,
+        onTagged: function(doclet, tag) {
+            // use an empty string so JSDoc can support `@inheritdoc Foo#bar` in the future
+            doclet.inheritdoc = '';
+        }
+    },
     inner: {
         onTagged: function(doclet, tag) {
             setDocletScopeToTitle(doclet, tag);
@@ -782,12 +789,20 @@ var closureTags = exports.closureTags = {
     extends: cloneTagDef(baseTags.augments),
     final: cloneTagDef(baseTags.readonly),
     implements: cloneTagDef(baseTags.implements),
+    inheritdoc: cloneTagDef(baseTags.inheritdoc),
     interface: cloneTagDef(baseTags.interface, {
         canHaveName: false,
         mustNotHaveValue: true
     }),
     lends: cloneTagDef(baseTags.lends),
     license: cloneTagDef(baseTags.license),
+    // Closure Compiler only
+    override: {
+        mustNotHaveValue: true,
+        onTagged: function(doclet, tag) {
+            doclet.override = true;
+        }
+    },
     param: cloneTagDef(baseTags.param),
     private: {
         canHaveType: true,

test/fixtures/inheritdoctag.js
@@ -1,0 +1,33 @@
+/**
+ * Parent class.
+ * @class
+ */
+function Connection() {}
+
+/**
+ * Open the connection.
+ * @virtual
+ */
+Connection.prototype.open = function() {};
+
+/**
+ * Close the connection.
+ */
+Connection.prototype.close = function() {};
+
+/**
+ * Child class.
+ * @class
+ * @extends Connection
+ */
+function Socket() {}
+
+/** @inheritdoc */
+Socket.prototype.open = function() {};
+
+/**
+ * Close the socket.
+ * @param {string} message - A message explaining why the socket is being closed.
+ * @inheritdoc
+ */
+Socket.prototype.close = function() {};

test/fixtures/overridetag.js
@@ -1,0 +1,33 @@
+/**
+ * Parent class.
+ * @class
+ */
+function Connection() {}
+
+/**
+ * Open the connection.
+ * @virtual
+ */
+Connection.prototype.open = function() {};
+
+/**
+ * Close the connection.
+ */
+Connection.prototype.close = function() {};
+
+/**
+ * Child class.
+ * @class
+ * @extends Connection
+ */
+function Socket() {}
+
+/** @override */
+Socket.prototype.open = function() {};
+
+/**
+ * Close the socket.
+ * @param {string} message - A message explaining why the socket is being closed.
+ * @override
+ */
+Socket.prototype.close = function() {};

test/specs/tags/augmentstag.js
@@ -101,10 +101,10 @@
         expect(bazMethod2.inherits).toBe('Bar#method2');
     });
 
-    it('When the grandparent has a method, and the parent overrides it, the child should not say it overrides the grandparent', function() {
+    it('When the grandparent has a method, and the parent overrides it, the child should say it overrides the parent', function() {
         var bazMethod2 = docSet.getByLongname('Baz#method2')[0];
 
-        expect(bazMethod2.overrides).not.toBeDefined();
+        expect(bazMethod2.overrides).toBe('Bar#method2');
     });
 
     it('When an object is extended, and it overrides an ancestor property, the child does not include docs for the ancestor property.', function() {

test/specs/tags/inheritdoctag.js
@@ -1,0 +1,37 @@
+'use strict';
+
+describe('@inheritdoc tag', function() {
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/inheritdoctag.js');
+
+    function ignored($) {
+        return $.ignore !== true;
+    }
+
+    it('should cause the symbol to be documented', function() {
+        var open = docSet.getByLongname('Socket#open');
+
+        expect(open.length).toBe(2);
+        expect(open[0].ignore).toBe(true);
+        expect(open[1].ignore).not.toBeDefined();
+        expect(open[1].description).toBe('Open the connection.');
+    });
+
+    it('should cause all other tags to be ignored', function() {
+        var close = docSet.getByLongname('Socket#close').filter(ignored)[0];
+
+        expect(close.description).toBe('Close the connection.');
+        expect(close.params).not.toBeDefined();
+    });
+
+    it('should not say that the child symbol is abstract', function() {
+        var open = docSet.getByLongname('Socket#open').filter(ignored)[0];
+        var parentOpen = docSet.getByLongname('Connection#open')[0];
+
+        expect(parentOpen.virtual).toBe(true);
+        expect(open.virtual).not.toBeDefined();
+    });
+
+    xit('should only be available if the Closure dictionary is enabled', function() {
+        // TODO
+    });
+});

test/specs/tags/overridetag.js
@@ -1,0 +1,37 @@
+'use strict';
+
+describe('@override tag', function() {
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/overridetag.js');
+
+    function ignored($) {
+        return $.ignore !== true;
+    }
+
+    it('should cause the symbol to be documented', function() {
+        var open = docSet.getByLongname('Socket#open');
+
+        expect(open.length).toBe(2);
+        expect(open[0].ignore).toBe(true);
+        expect(open[1].ignore).not.toBeDefined();
+        expect(open[1].description).toBe('Open the connection.');
+    });
+
+    it('should cause all other tags to be ignored', function() {
+        var close = docSet.getByLongname('Socket#close').filter(ignored)[0];
+
+        expect(close.description).toBe('Close the connection.');
+        expect(close.params).not.toBeDefined();
+    });
+
+    it('should not say that the child symbol is abstract', function() {
+        var open = docSet.getByLongname('Socket#open').filter(ignored)[0];
+        var parentOpen = docSet.getByLongname('Connection#open')[0];
+
+        expect(parentOpen.virtual).toBe(true);
+        expect(open.virtual).not.toBeDefined();
+    });
+
+    xit('should only be available if the Closure dictionary is enabled', function() {
+        // TODO
+    });
+});

commit 2b6d2dee2f8a887b60170f280ab981438241fc8d

fix crash caused by 39e5008cfc84fe3f88d6e6e75be15241bbd671fe
This is clearly the correct fix, but for testing purposes, it's not clear how to get the index into this weird state. Parsing all of JSDoc will cause a crash while looking at the doclet for module:jsdoc/tag/dictionary.Dictionary. Parsing just lib/jsdoc/tag/dictionary.js seems to work fine.

lib/jsdoc/augment.js
@@ -25,9 +25,9 @@ function mapDependencies(index, propertyName) {
                 dependencies[name] = {};
                 if (hasOwnProp.call(doc, propertyName)) {
                     len = doc[propertyName].length;
-                }
-                for (var j = 0; j < len; ++j) {
-                    dependencies[name][doc[propertyName][j]] = true;
+                    for (var j = 0; j < len; j++) {
+                        dependencies[name][doc[propertyName][j]] = true;
+                    }
                 }
             }
         }