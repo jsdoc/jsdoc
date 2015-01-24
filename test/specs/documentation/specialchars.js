c61b9307b4124a3a11203ed8f743002146ed219f
Jeff Williams <jeffrey.l.williams@gmail.com>
2015-01-24 07:40:47 -0800

    set the correct longname on prototype members whose names are quoted (#888)

---
+++ test/specs/documentation/specialchars.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('longnames with special characters', function() {
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/specialchars.js');
+    var portNumber = docSet.getByLongname('Socket#\'port#number\'')[0];
+    var open = docSet.getByLongname('Socket#\'open~a.connection#now\'')[0];
+
+    // currently broken: https://github.com/jsdoc3/jsdoc/issues/890
+    xit('should use the correct longname for instance members of "this" whose names contain ' +
+        'scope punctuation', function() {
+        expect(portNumber).toBeDefined();
+    });
+
+    it('should use the correct longname for instance members of the prototype whose names ' +
+        'contain scope punctuation', function() {
+        expect(open).toBeDefined();
+    });
+});
