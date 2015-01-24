a93eab0b4266da1f1d024a72403b5f80f3a29256
Jeff Williams <jeffrey.l.williams@gmail.com>
2015-01-24 09:09:31 -0800

    correctly handle symbols named `prototype` (#891) Conflicts: 	test/specs/documentation/specialnames.js

--- test/specs/documentation/specialnames.js
+++ test/specs/documentation/specialnames.js
@@ -5,8 +5,18 @@ describe('documenting symbols with special names', function() {
     var name = docSet.getByLongname('hasOwnProperty').filter(function($) {
         return !($.undocumented);
     });
+    var proto = docSet.getByLongname('prototype')[0];
+    var protoValueOf = docSet.getByLongname('prototype.valueOf')[0];
 
     it('When a symbol is named "hasOwnProperty," the symbol should appear in the docs.', function() {
         expect(name.length).toEqual(1);
+    });
+
+    it('When a symbol is named "prototype", the symbol should appear in the docs.', function() {
+        expect(proto).toBeDefined();
+    });
+
+    it('When a symbol is named "prototype", its members are resolved correctly.', function() {
+        expect(protoValueOf).toBeDefined();
     });
 });
