e89ecd615f02855d50df008f5238c4e1717f2c00
Jeff Williams <jeffrey.l.williams@gmail.com>
2015-01-24 08:40:55 -0800

    resolve `this` for computed properties (#890)

--- test/specs/documentation/specialchars.js
+++ test/specs/documentation/specialchars.js
@@ -5,8 +5,7 @@ describe('longnames with special characters', function() {
     var portNumber = docSet.getByLongname('Socket#\'port#number\'')[0];
     var open = docSet.getByLongname('Socket#\'open~a.connection#now\'')[0];
 
-    // currently broken: https://github.com/jsdoc3/jsdoc/issues/890
-    xit('should use the correct longname for instance members of "this" whose names contain ' +
+    it('should use the correct longname for instance members of "this" whose names contain ' +
         'scope punctuation', function() {
         expect(portNumber).toBeDefined();
     });
