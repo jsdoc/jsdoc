17c58d76e5eeffc2384b8bf005b462c0940ef5c3
Jeff Williams <jeffrey.l.williams@gmail.com>
2015-01-24 08:50:09 -0800

    clean up fixtures; add some missing tests

--- test/specs/documentation/specialnames.js
+++ test/specs/documentation/specialnames.js
@@ -2,11 +2,31 @@
 
 describe('documenting symbols with special names', function() {
     var docSet = jasmine.getDocSetFromFile('test/fixtures/specialnames.js');
-    var name = docSet.getByLongname('hasOwnProperty').filter(function($) {
-        return !($.undocumented);
+    var construct = docSet.getByLongname('constructor')[0];
+    var constructToString = docSet.getByLongname('constructor.toString')[0];
+    var hasOwnProp = docSet.getByLongname('hasOwnProperty')[0];
+    var proto = docSet.getByLongname('prototype')[0];
+    var protoValueOf = docSet.getByLongname('prototype.valueOf')[0];
+
+    it('When a symbol is named "constructor", the symbol should appear in the docs.', function() {
+        expect(construct).toBeDefined();
+    });
+
+    it('When a symbol is named "constructor", its members are resolved correctly.', function() {
+        expect(constructToString).toBeDefined();
     });
 
     it('When a symbol is named "hasOwnProperty," the symbol should appear in the docs.', function() {
-        expect(name.length).toEqual(1);
+        expect(hasOwnProp).toBeDefined();
+    });
+
+    // currently broken: https://github.com/jsdoc3/jsdoc/issues/891
+    xit('When a symbol is named "prototype", the symbol should appear in the docs.', function() {
+        expect(proto).toBeDefined();
+    });
+
+    // currently broken: https://github.com/jsdoc3/jsdoc/issues/891
+    xit('When a symbol is named "prototype", its members are resolved correctly.', function() {
+        expect(protoValueOf).toBeDefined();
     });
 });
