c56969b59fa0cef9dc6f284479213cc832ac00a9
Jeff Williams <jeffrey.l.williams@gmail.com>
2015-03-05 15:08:22 -0800

    correctly infer when the `class` tag contains a class description (#935)
    Fixes a regression caused by f1354563965792b8dd183f5a5331f8fcc67ea319.

--- test/specs/tags/classdesctag.js
+++ test/specs/tags/classdesctag.js
@@ -4,6 +4,7 @@ describe('@classdesc tag', function() {
     var docSet = jasmine.getDocSetFromFile('test/fixtures/classdesctag.js');
     var foo = docSet.getByLongname('Foo')[0];
     var bar = docSet.getByLongname('Bar')[0];
+    var baz = docSet.getByLongname('Baz')[0];
 
     it('should add a classdesc property to the doclet with the description', function() {
         expect(foo.classdesc).toBe('A description of the class.');
@@ -11,5 +12,9 @@ describe('@classdesc tag', function() {
 
     it('should work when the @class and @constructor tags are also present, and @class has a value', function() {
         expect(bar.classdesc).toBe('A description of the class.');
+    });
+
+    it('should infer that a description after the @class tag is a classdesc if no @classdesc tag is present', function() {
+        expect(baz.classdesc).toBe('Description of the Baz class.');
     });
 });
