jsdoc/jsdoc

commit f1354563965792b8dd183f5a5331f8fcc67ea319

don't overwrite the `classdesc` tag's value when the `class` and `constructor` tags are also present (#806)

lib/jsdoc/tag/dictionary/definitions.js
@@ -259,18 +259,20 @@ var baseTags = exports.baseTags = {
         onTagged: function(doclet, tag) {
             doclet.addTag('kind', 'class');
 
-            // handle special case where both @class and @constructor tags exist in same doclet
-            if (tag.originalTitle === 'class') {
-                // multiple words after @class?
-                var looksLikeDesc = (tag.value || '').match(/\S+\s+\S+/);
-                if ( looksLikeDesc || /@construct(s|or)\b/i.test(doclet.comment) ) {
-                    // treat the @class tag as a @classdesc tag instead
-                    doclet.classdesc = tag.value;
-                    return;
-                }
+            // We treat the @class tag as a @classdesc tag if all of the following are true:
+            // - Both @class and @constructor tags are present
+            // - There's no @classdesc tag
+            // - There are multiple words after @class
+            if (tag.value &&
+                tag.originalTitle === 'class' &&
+                /@construct(?:s|or)\b/i.test(doclet.comment) &&
+                !/@classdesc\b/i.test(doclet.comment) &&
+                tag.value.match(/\S+\s+\S+/)) {
+                doclet.addTag('classdesc', tag.value);
             }
-
-            setDocletNameToValue(doclet, tag);
+            else {
+                setDocletNameToValue(doclet, tag);
+            }
         },
         synonyms: ['constructor']
     },

test/fixtures/classdesctag.js
@@ -1,7 +1,14 @@
+'use strict';
+
 /**
  * Asdf.
  * @class
  * @classdesc A description of the class.
  */
-function Foo () {
-}
+function Foo() {}
+
+/**
+ * @classdesc A description of the class.
+ * @class Bar
+ * @constructor
+ */

test/specs/tags/classdesctag.js
@@ -1,8 +1,15 @@
-describe("@classdesc tag", function() {
-    var docSet = jasmine.getDocSetFromFile('test/fixtures/classdesctag.js'),
-        doc = docSet.getByLongname('Foo')[0];
+'use strict';
 
-    it('adds a classdesc property to the doclet with the description', function() {
-        expect(doc.classdesc).toBe('A description of the class.');
+describe('@classdesc tag', function() {
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/classdesctag.js');
+    var foo = docSet.getByLongname('Foo')[0];
+    var bar = docSet.getByLongname('Bar')[0];
+
+    it('should add a classdesc property to the doclet with the description', function() {
+        expect(foo.classdesc).toBe('A description of the class.');
+    });
+
+    it('should work when the @class and @constructor tags are also present, and @class has a value', function() {
+        expect(bar.classdesc).toBe('A description of the class.');
     });
 });

commit 4713925d783c598288027fb361b316c25707447f

correctly infer when the `class` tag contains a class description (#935)
Fixes a regression caused by f1354563965792b8dd183f5a5331f8fcc67ea319.

lib/jsdoc/tag/dictionary/definitions.js
@@ -262,20 +262,19 @@ var baseTags = exports.baseTags = {
         onTagged: function(doclet, tag) {
             doclet.addTag('kind', 'class');
 
-            // We treat the @class tag as a @classdesc tag if all of the following are true:
-            // - Both @class and @constructor tags are present
-            // - There's no @classdesc tag
-            // - There are multiple words after @class
-            if (tag.value &&
-                tag.originalTitle === 'class' &&
-                /@construct(?:s|or)\b/i.test(doclet.comment) &&
-                !/@classdesc\b/i.test(doclet.comment) &&
-                tag.value.match(/\S+\s+\S+/)) {
-                doclet.addTag('classdesc', tag.value);
+            // handle special case where both @class and @constructor tags exist in same doclet
+            if (tag.originalTitle === 'class') {
+                // multiple words after @class?
+                var looksLikeDesc = (tag.value || '').match(/\S+\s+\S+/);
+                if ((looksLikeDesc || /@construct(s|or)\b/i.test(doclet.comment)) &&
+                    !/@classdesc\b/i.test(doclet.comment)) {
+                    // treat the @class tag as a @classdesc tag instead
+                    doclet.classdesc = tag.value;
+                    return;
+                }
             }
-            else {
-                setDocletNameToValue(doclet, tag);
-            }
+
+            setDocletNameToValue(doclet, tag);
         },
         synonyms: ['constructor']
     },

test/fixtures/classdesctag.js
@@ -12,3 +12,10 @@ function Foo() {}
  * @class Bar
  * @constructor
  */
+
+/**
+ * The Baz class.
+ * @class
+ * Description of the Baz class.
+ */
+function Baz() {}

test/specs/tags/classdesctag.js
@@ -4,6 +4,7 @@ describe('@classdesc tag', function() {
     var docSet = jasmine.getDocSetFromFile('test/fixtures/classdesctag.js');
     var foo = docSet.getByLongname('Foo')[0];
     var bar = docSet.getByLongname('Bar')[0];
+    var baz = docSet.getByLongname('Baz')[0];
 
     it('should add a classdesc property to the doclet with the description', function() {
         expect(foo.classdesc).toBe('A description of the class.');
@@ -11,5 +12,9 @@ describe('@classdesc tag', function() {
 
     it('should work when the @class and @constructor tags are also present, and @class has a value', function() {
         expect(bar.classdesc).toBe('A description of the class.');
+    });
+
+    it('should infer that a description after the @class tag is a classdesc if no @classdesc tag is present', function() {
+        expect(baz.classdesc).toBe('Description of the Baz class.');
     });
 });