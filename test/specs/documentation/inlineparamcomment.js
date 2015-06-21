7a0ec075215a303634e0987287778ad72f8113b6
Jeff Williams <jeffrey.l.williams@gmail.com>
2015-06-21 10:56:34 -0700

    attach comments to default/rest parameters (#1025)

--- test/specs/documentation/inlineparamcomment.js
+++ test/specs/documentation/inlineparamcomment.js
@@ -11,4 +11,21 @@ describe('inline comments on function parameters', function() {
 
         expect(loadDocSet).not.toThrow();
     });
+
+    if (jasmine.jsParser !== 'rhino') {
+        describe('ES 2015 only', function() {
+            var docSet2 = jasmine.getDocSetFromFile('test/fixtures/inlineparamcomment2.js');
+            var foo = docSet2.getByLongname('ns.foo')[0];
+
+            it('should attach inline comments to default parameters', function() {
+                expect(foo.params[0].type.names.length).toBe(1);
+                expect(foo.params[0].type.names[0]).toBe('string');
+            });
+
+            it('should attach inline comments to rest parameters', function() {
+                expect(foo.params[1].type.names.length).toBe(1);
+                expect(foo.params[1].type.names[0]).toBe('number');
+            });
+        });
+    }
 });
