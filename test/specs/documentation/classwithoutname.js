4822bca6c1bc4c1c1b43500e2ffbb041bc33906a
Jeff Williams <jeffrey.l.williams@gmail.com>
2015-08-19 09:50:46 -0700

    whitespace

--- test/specs/documentation/classwithoutname.js
+++ test/specs/documentation/classwithoutname.js
@@ -1,15 +1,15 @@
 'use strict';
 
 describe('class without a name', function() {
-	var docSet = jasmine.getDocSetFromFile('test/fixtures/classwithoutname.js').doclets
-		.filter(function(doclet) {
-			return doclet.name === '';
-		});
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/classwithoutname.js').doclets
+        .filter(function(doclet) {
+            return doclet.name === '';
+        });
 
-	it('When the doclet for a class has an empty name, it should also have an empty longname', function() {
-		expect(docSet).toBeDefined();
-		expect(docSet.length).toBe(1);
-		expect(docSet[0].description).toBe('Create an instance of MyClass.');
-		expect(docSet[0].longname).toBe('');
-	});
+    it('When the doclet for a class has an empty name, it should also have an empty longname', function() {
+        expect(docSet).toBeDefined();
+        expect(docSet.length).toBe(1);
+        expect(docSet[0].description).toBe('Create an instance of MyClass.');
+        expect(docSet[0].longname).toBe('');
+    });
 });
