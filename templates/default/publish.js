aeed4365b08cb7ab8fee03fe2ad0276d9d10b237
Marouen Mhiri <marouen.mhiri@vermisst-gefunden.de>
2017-01-09 13:20:34 +0100

    change navimenu classes to methods

--- templates/default/publish.js
+++ templates/default/publish.js
@@ -349,7 +349,7 @@ function buildNav(members) {
 
     nav += buildMemberNav(members.modules, 'Modules', {}, linkto);
     nav += buildMemberNav(members.externals, 'Externals', seen, linktoExternal);
-    nav += buildMemberNav(members.classes, 'Classes', seen, linkto);
+    nav += buildMemberNav(members.classes, 'Methods', seen, linkto);
     nav += buildMemberNav(members.events, 'Events', seen, linkto);
     nav += buildMemberNav(members.namespaces, 'Namespaces', seen, linkto);
     nav += buildMemberNav(members.mixins, 'Mixins', seen, linkto);
