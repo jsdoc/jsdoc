0e4f1a957555eb21cbdf43701bb0b703f164f10f
Jeff Williams <jeffrey.l.williams@gmail.com>
2017-07-07 10:54:55 -0700

    correctly document constructors and instance properties of ES2015 classes (#1182)

---
+++ test/specs/documentation/moduleclasses.js
@@ -0,0 +1,31 @@
+'use strict';
+
+describe('module classes', function() {
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/moduleclasses.js');
+    var bar = docSet.getByLongname('module:foo~Bar')[0];
+    var barBar = docSet.getByLongname('module:foo~Bar#bar')[0];
+    var baz = docSet.getByLongname('module:foo.Baz')[0];
+    var bazBaz = docSet.getByLongname('module:foo.Baz#baz')[0];
+
+    describe('inner classes', function() {
+        it('should merge the constructor doclet with the class doclet', function() {
+            expect(bar.description).toBe('Construct a Bar.');
+            expect(bar.classdesc).toBe('Bar class.');
+        });
+
+        it('should correctly mark the scope of instance properties', function() {
+            expect(barBar.scope).toBe('instance');
+        });
+    });
+
+    describe('exported classes', function() {
+        it('should merge the constructor doclet with the class doclet', function() {
+            expect(baz.description).toBe('Construct a Baz.');
+            expect(baz.classdesc).toBe('Baz class.');
+        });
+
+        it('should correctly mark the scope of instance properties', function() {
+            expect(bazBaz.scope).toBe('instance');
+        });
+    });
+});
