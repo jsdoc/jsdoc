9f8853acff4f260272086fcc6003477b70b6edbe
Jeff Williams <jeffrey.l.williams@gmail.com>
2017-07-07 14:59:07 -0700

    add hideconstructor tag (#952)

---
+++ test/specs/tags/hideconstructortag.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('@hideconstructor tag', function() {
+    var docSet = jasmine.getDocSetFromFile('test/fixtures/hideconstructortag.js');
+    var toaster = docSet.getByLongname('Toaster')[0];
+    var waffleIron = docSet.getByLongname('WaffleIron')[0];
+
+    it('should add a `hideconstructor` attribute to pre-ES2015 classes', function() {
+        expect(toaster.hideconstructor).toBe(true);
+    });
+
+    it('should add a `hideconstructor` attribute to ES2015 classes when the constructor is tagged',
+        function() {
+            expect(waffleIron.hideconstructor).toBe(true);
+        });
+});
