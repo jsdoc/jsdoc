b112480dee77ee9ae171a929d3da71d4c193e3eb
Jeff Williams <jeffrey.l.williams@gmail.com>
2017-07-12 19:03:46 -0700

    fix crash when a class property has no value (#1400)

--- test/specs/documentation/classproperties.js
+++ test/specs/documentation/classproperties.js
@@ -4,6 +4,7 @@ describe('class properties', function() {
     var docSet = jasmine.getDocSetFromFile('test/fixtures/classproperties.js');
     var b = docSet.getByLongname('A#b')[0];
     var c = docSet.getByLongname('A#c')[0];
+    var d = docSet.getByLongname('A#d')[0];
 
     it('should assign the correct name, memberof, and scope to class properties', function() {
         expect(b.name).toBe('b');
@@ -17,5 +18,12 @@ describe('class properties', function() {
         expect(c.memberof).toBe('A');
         expect(c.scope).toBe('instance');
         expect(c.access).toBe('private');
+    });
+
+    it('should assign the correct name, memberof, and scope to class properties with no value ' +
+        'assigned', function() {
+        expect(d.name).toBe('d');
+        expect(d.memberof).toBe('A');
+        expect(d.scope).toBe('instance');
     });
 });
