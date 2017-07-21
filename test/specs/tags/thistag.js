e33f48c4713890d9a33c57b44033eff4a6d6a54e
Jeff Williams <jeffrey.l.williams@gmail.com>
2017-07-21 14:04:53 -0700

    support the `this` tag correctly for Closure Compiler (#605)

--- test/specs/tags/thistag.js
+++ test/specs/tags/thistag.js
@@ -1,18 +1,60 @@
 'use strict';
 
 describe('@this tag', function() {
-    var docSet = jasmine.getDocSetFromFile('test/fixtures/thistag.js');
-    var setName = docSet.getByLongname('setName')[0];
-    var fooName = docSet.getByLongname('Foo#name')[0];
-
-    it('When a symbol has a @this tag, the doclet has a this property that is set to that value.', function() {
-        expect(setName.this).toBe('Foo');
+    afterEach(function() {
+        jasmine.restoreTagDictionary();
     });
 
-    it('When a this symbol is documented inside a function with a @this tag, the symbol is documented as a member of that tags value.', function() {
-        expect(typeof fooName).toBe('object');
-        expect(fooName.name).toBe('name');
-        expect(fooName.memberof).toBe('Foo');
-        expect(fooName.scope).toBe('instance');
+    describe('JSDoc tags', function() {
+        beforeEach(function() {
+            jasmine.replaceTagDictionary('jsdoc');
+        });
+
+        it('should add a `this` property set to the @this tag\'s value', function() {
+            var docSet = jasmine.getDocSetFromFile('test/fixtures/thistag.js');
+            var setName = docSet.getByLongname('setName')[0];
+
+            expect(setName.this).toBe('Foo');
+        });
+
+        it('should change the memberof for symbols like `this.foo`', function() {
+            var docSet = jasmine.getDocSetFromFile('test/fixtures/thistag.js');
+            var fooName = docSet.getByLongname('Foo#name')[0];
+
+            expect(typeof fooName).toBe('object');
+            expect(fooName.name).toBe('name');
+            expect(fooName.memberof).toBe('Foo');
+            expect(fooName.scope).toBe('instance');
+        });
+    });
+
+    describe('Closure Compiler tags', function() {
+        beforeEach(function() {
+            jasmine.replaceTagDictionary('closure');
+        });
+
+        it('should add a `this` property set to the @this tag\'s type expression', function() {
+            var docSet = jasmine.getDocSetFromFile('test/fixtures/thistag2.js');
+            var setName = docSet.getByLongname('setName')[0];
+
+            expect(setName.this).toBe('Foo');
+        });
+
+        it('should change the memberof for symbols like `this.foo`', function() {
+            var docSet = jasmine.getDocSetFromFile('test/fixtures/thistag2.js');
+            var fooName = docSet.getByLongname('Foo#name')[0];
+
+            expect(typeof fooName).toBe('object');
+            expect(fooName.name).toBe('name');
+            expect(fooName.memberof).toBe('Foo');
+            expect(fooName.scope).toBe('instance');
+        });
+
+        it('should work with type unions', function() {
+            var docSet = jasmine.getDocSetFromFile('test/fixtures/thistag2.js');
+            var getName = docSet.getByLongname('getName')[0];
+
+            expect(getName.this).toBe('(Foo|Bar)');
+        });
     });
 });
