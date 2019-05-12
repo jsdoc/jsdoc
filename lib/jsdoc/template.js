jsdoc/jsdoc

commit 25f506aa570b4bbe180a8e88bf1ee3e9f675e540

Replace `underscore` with `lodash`.

lib/jsdoc/template.js
@@ -1,13 +1,13 @@
 /**
- * Wrapper for underscore's template utility to allow loading templates from files.
+ * Wrapper for Lodash's template utility to allow loading templates from files.
  * @module jsdoc/template
  */
-const _ = require('underscore');
+const _ = require('lodash');
 const fs = require('jsdoc/fs');
 const path = require('path');
 
 /**
- * Underscore template helper.
+ * Template helper.
  */
 class Template {
     /**

cli.js
@@ -47,7 +47,7 @@ module.exports = (() => {
 
     // TODO: docs
     cli.loadConfig = () => {
-        const _ = require('underscore');
+        const _ = require('lodash');
         const args = require('jsdoc/opts/args');
         const Config = require('jsdoc/config');
         let config;

lib/jsdoc/doclet.js
@@ -1,7 +1,7 @@
 /**
  * @module jsdoc/doclet
  */
-const _ = require('underscore');
+const _ = require('lodash');
 const jsdoc = {
     env: require('jsdoc/env'),
     name: require('jsdoc/name'),

lib/jsdoc/name.js
@@ -2,7 +2,7 @@
  * A collection of functions relating to JSDoc symbol name manipulation.
  * @module jsdoc/name
  */
-const _ = require('underscore');
+const _ = require('lodash');
 const escape = require('escape-string-regexp');
 
 const hasOwnProp = Object.prototype.hasOwnProperty;

lib/jsdoc/opts/argparser.js
@@ -2,7 +2,7 @@
  * Parse the command line arguments.
  * @module jsdoc/opts/argparser
  */
-const _ = require('underscore');
+const _ = require('lodash');
 const util = require('util');
 
 const hasOwnProp = Object.prototype.hasOwnProperty;

lib/jsdoc/tag/dictionary/definitions.js
@@ -2,7 +2,7 @@
  * Define tags that are known in JSDoc.
  * @module jsdoc/tag/dictionary/definitions
  */
-const _ = require('underscore');
+const _ = require('lodash');
 const jsdoc = {
     env: require('jsdoc/env'),
     name: require('jsdoc/name'),

plugins/eventDumper.js
@@ -3,11 +3,10 @@
  *
  * @module plugins/eventDumper
  */
-const _ = require('underscore');
+const _ = require('lodash');
 const doop = require('jsdoc/util/doop');
 const dump = require('jsdoc/util/dumper').dump;
 const env = require('jsdoc/env');
-const util = require('util');
 
 const conf = env.conf.eventDumper || {};
 
@@ -68,7 +67,7 @@ function cleanse(e) {
 
     Object.keys(e).forEach(prop => {
         // by default, don't stringify properties that contain an array of functions
-        if (!conf.includeFunctions && util.isArray(e[prop]) && e[prop][0] &&
+        if (!conf.includeFunctions && Array.isArray(e[prop]) && e[prop][0] &&
             String(typeof e[prop][0]) === 'function') {
             result[prop] = `function[${e[prop].length}]`;
         }

test/fixtures/eventorder.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _ = require('underscore');
+var _ = require('lodash');
 
 /**
  * Socket class.

commit 301750bb548fd8849f1a4fc3be9e4dce4c445653

Fix template breakage caused by 25f506aa570b4bbe180a8e88bf1ee3e9f675e540.

lib/jsdoc/template.js
@@ -31,7 +31,7 @@ class Template {
      * @return {function} Returns template closure.
      */
     load(file) {
-        return _.template(fs.readFileSync(file, 'utf8'), null, this.settings);
+        return _.template(fs.readFileSync(file, 'utf8'), this.settings);
     }
 
     /**