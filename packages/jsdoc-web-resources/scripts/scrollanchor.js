ecd3d49a5b0a7b92be161111807d26fbaa890ac4
Jeff Williams <jeffrey.l.williams@gmail.com>
2019-05-20 15:30:20 -0700

    add/update license info; fix more Gulpfile brokenness

--- packages/jsdoc-web-resources/scripts/scrollanchor.js
+++ packages/jsdoc-web-resources/scripts/scrollanchor.js
@@ -1,15 +1,30 @@
+/*
+    Copyright 2014-2019 Google LLC
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
 /* global document, window */
 // Function to prevent the top navbar from obscuring the page content.
 (function() {
     // timeout for scrolling the window
-    const TIMEOUT = 5;
+    var TIMEOUT = 5;
     // top navbar height
-    const TOP_OFFSET = 50;
+    var TOP_OFFSET = 50;
 
     function scrollTo(hash) {
-        const element = document.getElementById(hash.replace(/^#/, ''));
-        let elementOffset;
-        let rect;
+        var element = document.getElementById(hash.replace(/^#/, ''));
+        var elementOffset;
+        var rect;
 
         if (element) {
             rect = element.getBoundingClientRect();
@@ -22,7 +37,7 @@
     }
 
     window.addEventListener('load', function() {
-        const currentHash = window.location.hash;
+        var currentHash = window.location.hash;
 
         // if we're loading a URL with an anchor, scroll appropriately
         if (currentHash && currentHash !== '#') {
