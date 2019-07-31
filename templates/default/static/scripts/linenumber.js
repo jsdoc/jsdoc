5332ef3bed0e76a6c2bf362003852ba5c9ef5715
Titouan Le Floch Riche <titouan.leflochriche@outlook.com>
2019-07-31 17:11:00 +0200

    Add the possibility to highlight multiple lines when using an link to a file and line number by using <a href='your file#lineXtoY' (x is the first line and y the last one to highlight).

--- templates/default/static/scripts/linenumber.js
+++ templates/default/static/scripts/linenumber.js
@@ -7,9 +7,15 @@
     let lines;
     let totalLines;
     let anchorHash;
+    let from;
+    let to;
 
     if (source && source[0]) {
         anchorHash = document.location.hash.substring(1);
+        if (anchorHash.indexOf("to") != -1) {
+            from = anchorHash.split('to')[0].split('line')[1];
+            to = anchorHash.split('to')[1];
+        }
         lines = source[0].getElementsByTagName('li');
         totalLines = lines.length;
 
@@ -17,7 +23,7 @@
             lineNumber++;
             lineId = `line${lineNumber}`;
             lines[i].id = lineId;
-            if (lineId === anchorHash) {
+            if (lineId === anchorHash || (lineNumber >= parseInt(from) && lineNumber <= parseInt(to))) {
                 lines[i].className += ' selected';
             }
         }
