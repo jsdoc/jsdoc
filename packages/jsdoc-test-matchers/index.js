a912012bba443836cf509e1f755c2c9de151cba8
Jeff Williams <jeffrey.l.williams@gmail.com>
2019-10-09 13:05:40 -0400

    feat(jsdoc-test-matchers): add `toBeInstanceOf` matcher

--- packages/jsdoc-test-matchers/index.js
+++ packages/jsdoc-test-matchers/index.js
@@ -8,5 +8,27 @@ addMatchers({
     },
     toBeErrorOfType(other, value) {
         return value instanceof Error && value.name === other;
+    },
+    toBeInstanceOf(other, value) {
+        let otherName;
+        let valueName;
+
+        if (typeof value !== 'object') {
+            throw new TypeError(`Expected object value, got ${typeof value}`);
+        }
+
+        valueName = value.constructor.name;
+
+        // Class name.
+        if (typeof other === 'string') {
+            otherName = other;
+        // Class constructor.
+        } else if (typeof other === 'function') {
+            otherName = other.name;
+        } else {
+            otherName = other.constructor.name;
+        }
+
+        return valueName === otherName;
     }
 });
