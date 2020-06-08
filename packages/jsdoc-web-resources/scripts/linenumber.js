e8375e6dd708686dff6446e08dfb1246eb1f2e32
Jeff Williams <jeffrey.l.williams@gmail.com>
2020-06-07 17:41:31 -0700

    chore: update copyright year

--- packages/jsdoc-web-resources/scripts/linenumber.js
+++ packages/jsdoc-web-resources/scripts/linenumber.js
@@ -1,5 +1,5 @@
 /*
-    Copyright 2014-2019 Google LLC
+    Copyright 2014-2020 Google LLC
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
