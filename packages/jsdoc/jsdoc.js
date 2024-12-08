4b8be6bf68a9ce85b32e0a7c2e964b1e14e83435
Jeff Williams <jeffrey.l.williams@gmail.com>
2024-12-07 21:15:11 -0800

    refactor: more CLI streamlining

--- packages/jsdoc/jsdoc.js
+++ packages/jsdoc/jsdoc.js
@@ -15,9 +15,75 @@
   limitations under the License.
 */
 
-import * as cli from './cli.js';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
+import Engine from '@jsdoc/cli';
+import stripBom from 'strip-bom';
+
+async function createEngine() {
+  const packageJsonPath = fileURLToPath(new URL('package.json', import.meta.url));
+  // Allow this to throw; if we can't read our own package file, something is really wrong.
+  const packageInfo = JSON.parse(stripBom(fs.readFileSync(packageJsonPath, 'utf8')));
+  const revision = new Date(parseInt(packageInfo.revision, 10));
+  const { version } = packageInfo;
+  const engine = new Engine({ revision, version });
+
+  engine.configureLogger();
+  await engine.loadConfig();
+
+  return engine;
+}
+
+function logStart(engine) {
+  const { env, log } = engine;
+
+  log.debug(engine.versionDetails);
+  log.debug('Environment info: %j', {
+    env: {
+      conf: env.config,
+      opts: env.options,
+    },
+  });
+}
+
+function logFinish(engine) {
+  let delta;
+  let deltaSeconds;
+  const { env, log } = engine;
+
+  if (env.run.finish && env.run.start) {
+    delta = env.run.finish.getTime() - env.run.start.getTime();
+    deltaSeconds = (delta / 1000).toFixed(2);
+    log.info(`Finished running in ${deltaSeconds} seconds.`);
+  }
+}
+
+function runCommand(engine) {
+  let cmd;
+  const { options } = engine.env;
+
+  logStart(engine);
+
+  // If we already need to exit with an error, don't do any more work.
+  if (engine.shouldExitWithError) {
+    cmd = () => Promise.resolve(1);
+  } else if (options.help) {
+    cmd = () => engine.printHelp();
+  } else if (options.version) {
+    cmd = () => engine.printVersion();
+  } else {
+    cmd = () => engine.generate();
+  }
+
+  return cmd().then((errorCode) => {
+    logFinish(engine);
+    engine.exit(errorCode || 0);
+  });
+}
 
 (async () => {
-  await cli.initialize();
-  await cli.runCommand();
+  const engine = await createEngine();
+
+  await runCommand(engine);
 })();
