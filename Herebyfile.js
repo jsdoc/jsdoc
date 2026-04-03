757008055a925f0dddc974529c1de29074edd84e
Jeff Williams <jeffrey.l.williams@gmail.com>
2026-04-02 20:12:09 -0700

    chore(jsdoc-web-assets): add missing packages and npm scripts
    Specifically, from https://github.com/hegemonic/jsdoc-baseline.

--- Herebyfile.js
+++ Herebyfile.js
@@ -14,20 +14,223 @@
   limitations under the License.
 */
 
-import path from 'node:path';
+import { promises as fs } from 'node:fs';
+import { basename, join } from 'node:path';
 
+import * as esbuild from 'esbuild';
+import { sassPlugin } from 'esbuild-sass-plugin';
+import escapeStringRegexp from 'escape-string-regexp';
 import { execa } from 'execa';
 import { task } from 'hereby';
+import { glob } from 'tinyglobby';
 
 import runTests from './packages/jsdoc/test/index.js';
 
 const BIN_DIR = 'node_modules/.bin';
+const EXECA_OUT = {
+  stdout: 'inherit',
+  stderr: 'inherit',
+};
+const WEB_ASSETS = 'packages/jsdoc-web-assets';
 
-const sourceGlob = ['*.cjs', '*.js', 'packages/**/*/*.cjs', 'packages/**/*.js'];
+const sourceGlob = {
+  css: [],
+  js: {
+    minify: [join(WEB_ASSETS, 'scripts/index.js')],
+  },
+  lint: ['*.cjs', '*.js', 'packages/**/*/*.cjs', 'packages/**/*.js'],
+  sass: [join(WEB_ASSETS, 'styles/baseline.scss')],
+};
+
+const target = {
+  // TODO: Add JS path so we can delete .js.map files.
+  css: join(WEB_ASSETS, 'static'),
+};
 
 function bin(name) {
-  return path.join(BIN_DIR, name);
+  return join(BIN_DIR, name);
 }
+
+function esbuildTextReplace(options) {
+  options ??= { filter: /.*/, contentFilter: /.*/, replacers: [] };
+
+  function applyReplacers(source, replacers) {
+    let contents = source.slice();
+
+    replacers?.forEach(({ regexp, replacement }) => {
+      contents = contents.replaceAll(regexp, replacement);
+    });
+
+    return contents;
+  }
+
+  return {
+    name: 'textReplace',
+    setup(build) {
+      build.onLoad({ filter: options.filter, namespace: options.namespace }, async ({ path }) => {
+        let contents;
+        let source;
+
+        source = await fs.readFile(path, 'utf8');
+        if (!options.contentFilter || options.contentFilter.test(source)) {
+          contents = applyReplacers(source, options?.replacers);
+        } else {
+          contents = source;
+        }
+
+        return { contents };
+      });
+    },
+  };
+}
+
+const esbuildDevOptions = {
+  css: {
+    metafile: true,
+    minify: false,
+  },
+  js: {
+    metafile: true,
+    minify: false,
+    // TODO: figure out why this isn't working as expected in Chrome
+    // sourcemap: true,
+  },
+};
+
+const esbuildOptions = {
+  css: {
+    bundle: true,
+    minify: true,
+    outfile: join(WEB_ASSETS, 'static/css/baseline.css'),
+    plugins: [
+      sassPlugin({
+        embedded: true,
+      }),
+    ],
+  },
+  js: {
+    bundle: true,
+    minify: true,
+    outfile: join(WEB_ASSETS, 'static/scripts/jsdoc.js'),
+    plugins: [
+      esbuildTextReplace({
+        filter: /\/webawesome\/dist\/chunks\/.+\.js$/,
+        contentFilter: new RegExp(escapeStringRegexp('src/components/icon/library.system.ts')),
+        replacers: [
+          {
+            // Remove unused data from JS bundle.
+            //
+            // We want to match everything up to the next `var`. RE2 doesn't support negative
+            // lookahead, so we fake it.
+            regexp: /.*?\s*(var \S+)(?:[^v]|v[^a]|va[^r]|var[^\s])*/gs,
+            replacement: `export $1 = {};`,
+          },
+        ],
+      }),
+    ],
+    target: ['es2020'],
+  },
+};
+
+function copyTo(dest) {
+  return async (file) => {
+    const out = join(dest, basename(file));
+
+    await fs.copyFile(file, out);
+  };
+}
+
+async function removeMaps(filepath) {
+  let files;
+
+  if (!filepath) {
+    throw new Error('You must specify a filepath in which to remove .map files.');
+  }
+
+  files = await glob(join(filepath, '**/*.map'));
+
+  await Promise.all(files.map((file) => fs.rm(file)));
+}
+
+const cssStatic = task({
+  name: 'css-static',
+  run: async () => {
+    const files = await glob(sourceGlob.css);
+
+    await Promise.all(files.map(copyTo(target.css)));
+  },
+});
+
+// TODO: Rename some of these so we don't have, e.g., both `js-build` and `build-js`.
+const jsBuild = task({
+  name: 'js-build',
+  run: async () => {
+    const files = await glob(sourceGlob.js.minify);
+
+    const result = await esbuild.build({
+      ...esbuildDevOptions.js,
+      ...esbuildOptions.js,
+      entryPoints: files,
+    });
+
+    await fs.writeFile('meta.json', JSON.stringify(result.metafile));
+  },
+});
+
+const jsMinify = task({
+  name: 'js-minify',
+  run: async () => {
+    const files = await glob(sourceGlob.js.minify);
+
+    return esbuild.build({
+      ...esbuildOptions.js,
+      entryPoints: files,
+      metafile: true,
+    });
+  },
+});
+
+const sassBuild = task({
+  name: 'sass-build',
+  run: async () => {
+    const files = await glob(sourceGlob.css.concat(sourceGlob.sass));
+
+    return esbuild.build({
+      ...esbuildDevOptions.css,
+      ...esbuildOptions.css,
+      entryPoints: files,
+    });
+  },
+});
+
+export const css = task({
+  name: 'build-css',
+  run: async () => {
+    const files = await glob(sourceGlob.css.concat(sourceGlob.sass));
+
+    await removeMaps(target.css);
+
+    return esbuild.build({
+      ...esbuildOptions.css,
+      entryPoints: files,
+    });
+  },
+});
+
+export const dev = task({
+  name: 'build-dev',
+  dependencies: [cssStatic, jsBuild, sassBuild],
+});
+
+export const js = task({
+  name: 'build-js',
+  dependencies: [jsMinify],
+});
+
+export const build = task({
+  name: 'build',
+  dependencies: [css, js],
+});
 
 export const coverage = task({
   name: 'coverage',
@@ -45,20 +248,14 @@ export const coverage = task({
 export const dependencyCleanup = task({
   name: 'dependency-cleanup',
   run: async () => {
-    await execa(bin('knip'), [], {
-      stdout: 'inherit',
-      stderr: 'inherit',
-    });
+    await execa(bin('knip'), [], EXECA_OUT);
   },
 });
 
 export const dependencyEngines = task({
   name: 'dependency-engines',
   run: async () => {
-    await execa(bin('installed-check'), ['--no-include-workspace-root'], {
-      stdout: 'inherit',
-      stderr: 'inherit',
-    });
+    await execa(bin('installed-check'), ['--no-include-workspace-root'], EXECA_OUT);
   },
 });
 
@@ -96,7 +293,7 @@ export const licenseCheck = task({
 export const lint = task({
   name: 'lint',
   run: async () => {
-    await execa(bin('eslint'), [...sourceGlob], { stdout: 'inherit', stderr: 'inherit' });
+    await execa(bin('eslint'), [...sourceGlob.lint], EXECA_OUT);
   },
 });
 
